7526aaaa68a7381d184cac12b383a0e7bdc7d783
Zikrur Rahman <zikrur.rahman@hmcts.net>
2022-01-11 15:47:17 +0000

    respondent notification

---
+++ src/main/java/uk/gov/hmcts/reform/civil/config/properties/notification/NotificationsProperties.java
@@ -0,0 +1,18 @@
+package uk.gov.hmcts.reform.civil.config.properties.notification;
+
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+@Validated
+@Data
+public class NotificationsProperties {
+
+    @NotEmpty
+    private String govNotifyApiKey;
+
+    @NotEmpty
+    private String generalApplicationRespondentsEmailTemplate;
+
+}
