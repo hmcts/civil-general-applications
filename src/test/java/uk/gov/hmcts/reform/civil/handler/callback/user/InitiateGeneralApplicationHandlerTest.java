3ecece949c8c7719eebc6066f6eb5fa263964f40
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-01-19 13:17:42 +0000

    CIV-660 Add a test for single application type and multiple application type

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/InitiateGeneralApplicationHandlerTest.java
@@ -10,11 +10,9 @@ import uk.gov.hmcts.reform.ccd.client.model.SubmittedCallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.enums.BusinessProcessStatus;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
-import uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAApplicationType;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAInformOtherParty;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAUrgencyRequirement;
@@ -22,19 +20,16 @@ import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
-import static java.lang.String.format;
 import static java.time.LocalDate.EPOCH;
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INITIATE_GENERAL_APPLICATION;
-import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
 import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.EXTEND_TIME;
+import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.SUMMARY_JUDGEMENT;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
 @SuppressWarnings({"checkstyle:EmptyLineSeparator", "checkstyle:Indentation"})
@@ -51,26 +46,36 @@ class InitiateGeneralApplicationHandlerTest extends BaseCallbackHandlerTest {
     @Value("${civil.response-pack-url}")
     private static final String STRING_CONSTANT = "this is a string";
     private static final LocalDate APP_DATE_EPOCH = EPOCH;
-    private static final String CONFIRMATION_SUMMARY = "<br/><p> Your Court will make a decision on %s."
-        + "<ul> %s </ul>"
-        + "</p> %s"
-        + " %s ";
-    private static final String URGENT_APPLICATION = "<p> You have marked this application as urgent. </p>";
-    private static final String PARTY_NOTIFIED = "<p> The other %s legal representative %s "
-        + "that you have submitted this application.";
+    private static final String CONFIRMATION_SINGEL = "<br/><p> Your Court will make a decision on this application."
+        + "<ul> <li>EXTEND_TIME</li> </ul>"
+        + "</p> <p> You have marked this application as urgent. </p> <p> The other party's legal representative "
+        + "has been notified that you have submitted this application. ";
+    private static final String CONFIRMATION = "<br/><p> Your Court will make a decision on these applications."
+        + "<ul> <li>EXTEND_TIME</li><li>SUMMARY_JUDGEMENT</li> </ul>"
+        + "</p> <p> You have marked this application as urgent. </p> <p> The other party's legal representative "
+        + "has been notified that you have submitted this application. ";
+
 
     private CaseData getEmptyTestCase(CaseData caseData) {
         return caseData.toBuilder()
             .build();
     }
 
-    private CaseData getReadyTestCaseData(CaseData caseData) {
-        GeneralApplication application = GeneralApplication.builder()
-            .generalAppType(GAApplicationType.builder()
-                                .types(singletonList(EXTEND_TIME))
-                                .build())
+    private CaseData getReadyTestCaseData(CaseData caseData, boolean multipleGenAppTypes) {
+        GeneralApplication.GeneralApplicationBuilder builder = GeneralApplication.builder();
+        if (multipleGenAppTypes) {
+            builder.generalAppType(GAApplicationType.builder()
+                    .types(Arrays.asList(EXTEND_TIME, SUMMARY_JUDGEMENT))
+                    .build());
+        } else {
+            builder.generalAppType(GAApplicationType.builder()
+                    .types(singletonList(EXTEND_TIME))
+                    .build());
+        }
+        GeneralApplication application = builder
+
             .generalAppInformOtherParty(GAInformOtherParty.builder()
-                                            .isWithNotice(NO)
+                                            .isWithNotice(YES)
                                             .reasonsForWithoutNotice(STRING_CONSTANT)
                                             .build())
             .generalAppUrgencyRequirement(GAUrgencyRequirement.builder()
@@ -89,31 +94,6 @@ class InitiateGeneralApplicationHandlerTest extends BaseCallbackHandlerTest {
             .build();
     }
 
-    private CaseData getStartedTestCaseData(CaseData caseData) {
-        GeneralApplication application = GeneralApplication.builder()
-            .generalAppType(GAApplicationType.builder()
-                                .types(singletonList(EXTEND_TIME))
-                                .build())
-            .generalAppInformOtherParty(GAInformOtherParty.builder()
-                                            .isWithNotice(NO)
-                                            .reasonsForWithoutNotice(STRING_CONSTANT)
-                                            .build())
-            .generalAppUrgencyRequirement(GAUrgencyRequirement.builder()
-                                              .generalAppUrgency(YES)
-                                              .reasonsForUrgency(STRING_CONSTANT)
-                                              .urgentAppConsiderationDate(APP_DATE_EPOCH)
-                                              .build())
-            .isMultiParty(YesOrNo.NO)
-            .businessProcess(BusinessProcess.builder()
-                                 .status(BusinessProcessStatus.STARTED)
-                                 .build())
-            .build();
-        return getEmptyTestCase(caseData)
-            .toBuilder()
-            .generalApplications(wrapElements(application))
-            .build();
-    }
-
     @Nested
     class SubmittedCallback {
 
@@ -123,69 +103,23 @@ class InitiateGeneralApplicationHandlerTest extends BaseCallbackHandlerTest {
         }
 
         @Test
-        void sholudReturnFirstReadyGeneralApplicationElement() {
-            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().build());
-            List<Element<GeneralApplication>> generalApplications = caseData.getGeneralApplications();
-            Optional<Element<GeneralApplication>> generalApplicationElementOptional = generalApplications.stream()
-                .filter(app -> app.getValue().getBusinessProcess().getStatus() == BusinessProcessStatus.READY
-                    && app.getValue().getBusinessProcess().getProcessInstanceId() == null).findFirst();
-            if (generalApplicationElementOptional.isPresent()) {
-                GeneralApplication generalApplicationElement = generalApplicationElementOptional.get().getValue();
-                assertThat(generalApplicationElement).isNotNull();
-            }
-        }
-
-        @Test
-        void sholudNotReturnFirstReadyGeneralApplicationElement() {
-            CaseData caseData = getStartedTestCaseData(CaseDataBuilder.builder().build());
-            List<Element<GeneralApplication>> generalApplications = caseData.getGeneralApplications();
-            Optional<Element<GeneralApplication>> generalApplicationElementOptional = generalApplications.stream()
-                .filter(app -> app.getValue().getBusinessProcess().getStatus() == BusinessProcessStatus.READY
-                    && app.getValue().getBusinessProcess().getProcessInstanceId() == null).findFirst();
-            if (generalApplicationElementOptional.isPresent()) {
-                GeneralApplication generalApplicationElement = generalApplicationElementOptional.get().getValue();
-                assertThat(generalApplicationElement).isNull();
-            }
-        }
-
-        @Test
-        void shouldReturnExpectedSubmittedCallbackResponse() {
-            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().build());
-            List<Element<GeneralApplication>> generalApplications = caseData.getGeneralApplications();
-            Optional<Element<GeneralApplication>> generalApplicationElementOptional = generalApplications.stream()
-                .filter(app -> app.getValue().getBusinessProcess().getStatus() == BusinessProcessStatus.READY
-                    && app.getValue().getBusinessProcess().getProcessInstanceId() == null).findFirst();
+        void shouldReturnBuildConfirmationForSingleApplicationType() {
+            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().build(), false);
             CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
-            SubmittedCallbackResponse response = (SubmittedCallbackResponse) handler.handle(params);
 
-            if (generalApplicationElementOptional.isPresent()) {
-                GeneralApplication generalApplicationElement = generalApplicationElementOptional.get().getValue();
-                List<GeneralApplicationTypes> types = generalApplicationElement.getGeneralAppType().getTypes();
-                String collect = types.stream().map(appType -> "<li>" + appType + "</li>")
-                    .collect(Collectors.joining());
-                boolean isApplicationUrgent = Optional.of(generalApplicationElement.getGeneralAppUrgencyRequirement()
-                                                              .getGeneralAppUrgency() == YesOrNo.YES).orElse(true);
-                boolean isMultiParty = Optional.of(generalApplicationElement.getIsMultiParty()
-                                                       == YesOrNo.YES).orElse(true);
-                boolean isNotified = Optional.of(generalApplicationElement.getGeneralAppInformOtherParty()
-                                                     .getIsWithNotice()
-                                                     == YesOrNo.YES).orElse(true);
-                String lastLine = format(PARTY_NOTIFIED, isMultiParty ? "parties'" : "party's",
-                                         isNotified ? "has been notified" : "has not been notified"
-                );
-                String body = format(
-                    CONFIRMATION_SUMMARY,
-                    types.size() == 1 ? "this application" : "these applications",
-                    collect,
-                    isApplicationUrgent ? URGENT_APPLICATION : " ",
-                    lastLine
-                );
-                assertThat(response).usingRecursiveComparison().isEqualTo(
-                    SubmittedCallbackResponse.builder()
-                        .confirmationHeader("# You have made an application")
-                        .confirmationBody(body)
-                        .build());
-            }
+            var response = (SubmittedCallbackResponse) handler.handle(params);
+            assertThat(response).isNotNull();
+            assertThat(response.getConfirmationBody()).isEqualTo(CONFIRMATION_SINGEL);
+        }
+
+        @Test
+        void shouldReturnBuildConfirmationForMultipleApplicationType() {
+            CaseData caseData = getReadyTestCaseData(CaseDataBuilder.builder().build(), true);
+            CallbackParams params = callbackParamsOf(caseData, SUBMITTED);
+
+            var response = (SubmittedCallbackResponse) handler.handle(params);
+            assertThat(response).isNotNull();
+            assertThat(response.getConfirmationBody()).isEqualTo(CONFIRMATION);
         }
     }
 }
