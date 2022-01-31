c3a594634185db6fb6d248cca7a995a37fdaf38d
Karthick Mohanasundaram <karthick.mohanasundaram@HMCTS.NET>
2022-01-31 10:57:46 +0000

    CIV-206 Assign Applicant and Respondent roles to correct solicitor

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/caseassignment/AssignCaseToUserCallbackHandler.java
@@ -0,0 +1,120 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.caseassignment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDataContent;
+import uk.gov.hmcts.reform.ccd.client.model.Event;
+import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackException;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.CaseRole;
+import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.IdamUserDetails;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
+
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.ASSIGN_GA_ROLES;
+
+@Service
+@RequiredArgsConstructor
+public class AssignCaseToUserCallbackHandler extends CallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = List.of(ASSIGN_GA_ROLES);
+    public static final String TASK_ID = "AssigningOfRoles";
+
+    private final CoreCaseUserService coreCaseUserService;
+    private final CaseDetailsConverter caseDetailsConverter;
+    private final CoreCaseDataService coreCaseDataService;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_SUBMIT), this::assignSolicitorCaseRole
+        );
+    }
+
+    @Override
+    public String camundaActivityId(CallbackParams callbackParams) {
+        return TASK_ID;
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    private Boolean checkGAExitsWithSolicitorDetails(Element<GeneralApplication> generalApplication) {
+        return generalApplication.getValue().getApplicantSolicitor1UserDetails() != null
+            && generalApplication.getValue().getApplicant1OrganisationPolicy() != null
+            && generalApplication.getValue().getRespondent1OrganisationPolicy() != null
+            && generalApplication.getValue().getRespondentSolicitor1EmailAddress() != null;
+    }
+
+    private void setSolicitorEmailID(Element<GeneralApplication> generalApplication, IdamUserDetails userDetails) {
+        generalApplication.getValue().toBuilder()
+            .applicantSolicitor1UserDetails(IdamUserDetails.builder()
+                                                .email(userDetails.getEmail()).build()).build();
+    }
+
+    private CaseDataContent caseDataContent(StartEventResponse startEventResponse,
+                                            List<Element<GeneralApplication>> generalApplications) {
+        Map<String, Object> data = startEventResponse.getCaseDetails().getData();
+        data.put("generalApplications", generalApplications);
+
+        return CaseDataContent.builder()
+            .eventToken(startEventResponse.getToken())
+            .event(Event.builder().id(startEventResponse.getEventId()).build())
+            .data(data)
+            .build();
+    }
+
+    private CallbackResponse assignSolicitorCaseRole(CallbackParams callbackParams) {
+        CaseData caseData = caseDetailsConverter.toCaseData(callbackParams.getRequest().getCaseDetails());
+        String caseId = caseData.getCcdCaseReference().toString();
+
+        if (caseData.getGeneralApplications() != null) {
+            List<Element<GeneralApplication>> generalApplications = caseData.getGeneralApplications();
+            for (Element<GeneralApplication> generalApplication : generalApplications) {
+                if (checkGAExitsWithSolicitorDetails(generalApplication)) {
+                    IdamUserDetails userDetails = generalApplication.getValue().getApplicantSolicitor1UserDetails();
+                    String submitterId = userDetails.getId();
+                    String organisationId = generalApplication.getValue()
+                            .getApplicant1OrganisationPolicy().getOrganisation().getOrganisationID();
+
+                    coreCaseUserService.assignCase(caseId, submitterId, organisationId, CaseRole.APPLICANTSOLICITORONE);
+                    // create respondent solicitor
+
+                }
+            }
+            return updateCaseDate(callbackParams, generalApplications);
+        }
+
+        throw new CallbackException(String.format("AssignCaseToUserCallbackHandler::assignSolicitorCaseRole "
+                                                      + "NullPointer Exception : %s", caseId));
+
+    }
+
+    private CallbackResponse updateCaseDate(CallbackParams callbackParams,
+                                            List<Element<GeneralApplication>> generalApplications) {
+        Map<String, Object> output = callbackParams.getRequest().getCaseDetails().getData();
+        output.put("generalApplications", generalApplications);
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(output)
+            .build();
+    }
+}
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/caseassignment/AssignCaseToUserHandlerTest.java
@@ -0,0 +1,118 @@
+package uk.gov.hmcts.reform.civil.handler.callback.caseassignment;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uk.gov.hmcts.reform.ccd.model.Organisation;
+import uk.gov.hmcts.reform.ccd.model.OrganisationPolicy;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CallbackType;
+import uk.gov.hmcts.reform.civil.enums.BusinessProcessStatus;
+import uk.gov.hmcts.reform.civil.enums.CaseRole;
+import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
+import uk.gov.hmcts.reform.civil.handler.callback.camunda.caseassignment.AssignCaseToUserCallbackHandler;
+import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.civil.model.BusinessProcess;
+import uk.gov.hmcts.reform.civil.model.IdamUserDetails;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAApplicationType;
+import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
+import uk.gov.hmcts.reform.civil.service.CoreCaseUserService;
+
+import java.util.Map;
+
+import static java.util.Collections.singletonList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.SUMMARY_JUDGEMENT;
+
+@SpringBootTest(classes = {
+    AssignCaseToUserCallbackHandler.class,
+    JacksonAutoConfiguration.class,
+    CaseDetailsConverter.class
+})
+public class AssignCaseToUserHandlerTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    private AssignCaseToUserCallbackHandler assignCaseToUserHandler;
+
+    @MockBean
+    private CoreCaseUserService coreCaseUserService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private CallbackParams params;
+
+    private GeneralApplication caseData;
+
+    public static final Long CASE_ID = 1594901956117591L;
+
+    public static final String STRING_CONSTANT = "This is string";
+
+    @BeforeEach
+    void setup() {
+
+        GeneralApplication.GeneralApplicationBuilder builder = GeneralApplication.builder();
+        builder.generalAppType(GAApplicationType.builder()
+                                   .types(singletonList(SUMMARY_JUDGEMENT))
+                                   .build())
+            .applicantSolicitor1UserDetails(IdamUserDetails.builder()
+                                                .id("f5e5cc53-e065-43dd-8cec-2ad005a6b9a9")
+                                                .email("applicant@someorg.com")
+                                                .build())
+            .businessProcess(BusinessProcess.builder().status(BusinessProcessStatus.READY).build())
+            .applicant1OrganisationPolicy(OrganisationPolicy.builder()
+                                              .organisation(Organisation.builder().organisationID("OrgId1").build())
+                                              .build())
+            .respondent1OrganisationPolicy(OrganisationPolicy.builder()
+                                               .organisation(Organisation.builder().organisationID("OrgId2").build())
+                                               .build())
+
+            .build();
+
+        caseData = builder.build();
+
+        Map<String, Object> dataMap = objectMapper.convertValue(caseData, new TypeReference<>() {
+        });
+        params = callbackParamsOf(dataMap, CallbackType.ABOUT_TO_SUBMIT);
+    }
+
+    @Test
+    void shouldAssignCaseToApplicantSolicitorOne() {
+
+        assignCaseToUserHandler.handle(params);
+
+        verify(coreCaseUserService).assignCase(
+            CASE_ID.toString(),
+            caseData.getApplicantSolicitor1UserDetails().getId(),
+            "OrgId1",
+            CaseRole.APPLICANTSOLICITORONE
+        );
+    }
+
+    @Test
+    void shouldNotInvokeAssignUserToCaseForRole() {
+
+        when(coreCaseUserService
+                 .userWithCaseRoleExistsOnCase(STRING_CONSTANT, STRING_CONSTANT, CaseRole.APPLICANTSOLICITORONE))
+            .thenReturn(Boolean.TRUE);
+
+        assignCaseToUserHandler.handle(params);
+
+        verify(coreCaseUserService, never()).assignUserToCaseForRole(
+            anyString(),
+            anyString(),
+            anyString(),
+            any(CaseRole.class),
+            anyString()
+        );
+    }
+}
