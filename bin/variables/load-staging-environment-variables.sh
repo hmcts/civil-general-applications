ce68c105f00d549343cd171683415ab65f24db75
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-04-04 08:01:02 +0100

    updated xui igress host

--- bin/variables/load-staging-environment-variables.sh
+++ bin/variables/load-staging-environment-variables.sh
@@ -12,5 +12,5 @@ echo "export CCD_DEFINITION_STORE_API_BASE_URL=http://civil-ga-definition-store-
 echo "export CAMUNDA_BASE_URL=http://civil-ga-camunda-staging-aat.service.core-compute-aat.internal"
 
 # definition placeholders
-echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-general-applications.service.core-compute-aat.internal"
+echo "export CCD_DEF_CASE_SERVICE_BASE_URL=http://civil-service-civil-general-applications-staging.service.core-compute-aat.internal"
 echo "export CCD_DEF_GEN_APP_SERVICE_BASE_URL=http://civil-general-applications-staging.service.core-compute-aat.internal"
