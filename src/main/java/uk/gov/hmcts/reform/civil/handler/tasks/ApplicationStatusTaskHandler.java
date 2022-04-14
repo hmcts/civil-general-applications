23cab5bc4e0f410a60b34b70c7e5904822c1d90c
Zikrur Rahman <zikrur.rahman@hmcts.net>
2022-04-14 16:50:59 +0100

    adding todo and removing unused imports

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/ApplicationStatusTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/ApplicationStatusTaskHandler.java
@@ -5,8 +5,6 @@ import org.camunda.bpm.client.task.ExternalTask;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
-import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
-import uk.gov.hmcts.reform.civil.service.EventEmitterService;
 import uk.gov.hmcts.reform.civil.service.search.CaseReadyBusinessProcessSearchService;
 
 import java.util.List;
@@ -17,12 +15,11 @@ import java.util.List;
 public class ApplicationStatusTaskHandler implements BaseExternalTaskHandler {
 
     private final CaseReadyBusinessProcessSearchService caseSearchService;
-    private final CaseDetailsConverter caseDetailsConverter;
-    private final EventEmitterService eventEmitterService;
 
     @Override
     public void handleTask(ExternalTask externalTask) {
         List<CaseDetails> cases = caseSearchService.getCases();
         log.info("Job '{}' found {} case(s)", externalTask.getTopicName(), cases.size());
+        //TODO find case and handle
     }
 }
