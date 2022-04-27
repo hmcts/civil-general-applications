0508dc0ee09c4b69369f26a1ce1e8c7eebe57c57
dawidWalter <dawid.walter@hmcts.net>
2022-04-27 16:08:18 +0100

    Fix for CaseData new methods errors

--- src/main/java/uk/gov/hmcts/reform/civil/service/StateGeneratorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/StateGeneratorService.java
@@ -17,8 +17,19 @@ import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.REQUEST_M
 public class StateGeneratorService {
 
     public CaseState getCaseStateForEndJudgeBusinessProcess(CaseData data) {
-        GAJudgeDecisionOption decision = data.getJudicialDecisionOption();
-        String directionsText = data.getJudicialDecisionMakeOrderDirectionsText();
+        GAJudgeDecisionOption decision;
+        String directionsText;
+
+        if (data.getJudicialDecisionMakeOrder() != null) {
+            directionsText = data.getJudicialDecisionMakeOrder().getDirectionsText();
+        } else {
+            directionsText = null;
+        }
+        if (data.getJudicialDecision() != null) {
+            decision = data.getJudicialDecision().getDecision();
+        } else {
+            decision = null;
+        }
 
         if (decision == MAKE_AN_ORDER && !isBlank(directionsText)) {
             return AWAITING_DIRECTIONS_ORDER_DOCS;
