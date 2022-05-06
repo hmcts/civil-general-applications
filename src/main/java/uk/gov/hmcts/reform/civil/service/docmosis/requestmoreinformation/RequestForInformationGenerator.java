24246b95e0d2de70607ec0209726c45215e7d726
Maciej Nycz <maciej.nycz@version1.com>
2022-05-06 17:13:56 +0100

    Create PDF Service - request for information

---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/requestmoreinformation/RequestForInformationGenerator.java
@@ -0,0 +1,88 @@
+package uk.gov.hmcts.reform.civil.service.docmosis.requestmoreinformation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.docmosis.DocmosisDocument;
+import uk.gov.hmcts.reform.civil.model.docmosis.requestforinformation.RequestForInformation;
+import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
+import uk.gov.hmcts.reform.civil.model.documents.DocumentType;
+import uk.gov.hmcts.reform.civil.model.documents.PDF;
+import uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates;
+import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
+import uk.gov.hmcts.reform.civil.service.docmosis.TemplateDataGenerator;
+import uk.gov.hmcts.reform.civil.service.documentmanagement.DocumentManagementService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.REQUEST_FOR_INFORMATION;
+
+@Service
+@RequiredArgsConstructor
+public class RequestForInformationGenerator implements TemplateDataGenerator<RequestForInformation> {
+
+    private final DocumentManagementService documentManagementService;
+    private final DocumentGeneratorService documentGeneratorService;
+
+    public CaseDocument generate(CaseData caseData, String authorisation) {
+        RequestForInformation templateData = getTemplateData(caseData);
+
+        DocmosisTemplates docmosisTemplate = getDocmosisTemplate(caseData);
+
+        DocmosisDocument docmosisDocument = documentGeneratorService.generateDocmosisDocument(
+            templateData,
+            docmosisTemplate
+        );
+
+        return documentManagementService.uploadDocument(
+            authorisation,
+            new PDF(getFileName(docmosisTemplate, caseData), docmosisDocument.getBytes(),
+                    DocumentType.REQUEST_FOR_INFORMATION)
+        );
+    }
+
+    private String getFileName(DocmosisTemplates docmosisTemplate, CaseData caseData) {
+        return String.format(docmosisTemplate.getDocumentTitle(), caseData.getLegacyCaseReference());
+    }
+
+    @Override
+    public RequestForInformation getTemplateData(CaseData caseData) {
+        List<GeneralApplicationTypes> types = caseData.getGeneralAppType().getTypes();
+        String collect = types.stream()
+            .map(GeneralApplicationTypes::getDisplayedValue).collect(Collectors.joining(", "));
+
+        List<String> claimantNames = new ArrayList<>();
+        claimantNames.add(caseData.getClaimant1PartyName());
+        if (caseData.getClaimant2PartyName() != null) {
+            claimantNames.add(caseData.getClaimant2PartyName());
+        }
+        String claimantName = String.join(", ", claimantNames);
+
+        List<String> defendentNames = new ArrayList<>();
+        defendentNames.add(caseData.getDefendant1PartyName());
+        if (caseData.getDefendant2PartyName() != null) {
+            defendentNames.add(caseData.getDefendant2PartyName());
+        }
+        String defendantName = String.join(", ", defendentNames);
+
+        RequestForInformation.RequestForInformationBuilder requestForInformationBuilderBuilder =
+            RequestForInformation.builder()
+                .claimNumber(caseData.getCcdCaseReference().toString())
+                .applicationType(collect)
+                .claimantName(claimantName)
+                .defendantName(defendantName)
+                .judgeComments(caseData.getJudicialDecisionRequestMoreInfo().getJudgeRequestMoreInfoText())
+                .submittedOn(LocalDate.now().plusDays(14))
+                .dateBy(caseData.getJudicialDecisionRequestMoreInfo().getJudgeRequestMoreInfoByDate());
+
+        return requestForInformationBuilderBuilder.build();
+    }
+
+    private DocmosisTemplates getDocmosisTemplate(CaseData caseData) {
+        return REQUEST_FOR_INFORMATION;
+    }
+}
