e082980023a997d1374eb39cd2fb8f2f27b35655
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-05-10 11:28:17 +0100

    Update main.tf
    to add in app insights

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,18 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = var.location
+
+  tags = var.common_tags
+}
+
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-${var.component}-${var.env}"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
