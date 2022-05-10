68829ae1d6b7c9f8164eefa6be2091ab2cd52455
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-05-10 11:39:25 +0100

    Update variables.tf
    with appinsights location variable

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -15,3 +15,9 @@ variable "deployment_namespace" {}
 variable "common_tags" {
   type = "map"
 }
+
+variable "appinsights_location" {
+  type        = string
+  default     = "West Europe"
+  description = "Location for Application Insights"
+}
