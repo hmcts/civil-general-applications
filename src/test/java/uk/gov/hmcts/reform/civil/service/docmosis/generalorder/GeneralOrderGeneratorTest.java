8016265f216fc03bff1dfc9046d091a595c123c2
Maciej Nycz <maciej.nycz@version1.com>
2022-05-12 18:49:09 +0100

    add more test

---
+++ src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/generalorder/GeneralOrderGeneratorTest.java
@@ -0,0 +1,139 @@
+package uk.gov.hmcts.reform.civil.service.docmosis.generalorder;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes;
+import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.MappableObject;
+import uk.gov.hmcts.reform.civil.model.docmosis.DocmosisDocument;
+import uk.gov.hmcts.reform.civil.model.docmosis.generalorder.GeneralOrder;
+import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
+import uk.gov.hmcts.reform.civil.model.documents.DocumentType;
+import uk.gov.hmcts.reform.civil.model.documents.PDF;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
+import uk.gov.hmcts.reform.civil.service.documentmanagement.UnsecuredDocumentManagementService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.GENERAL_ORDER;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+    GeneralOrderGenerator.class,
+    JacksonAutoConfiguration.class,
+    CaseDetailsConverter.class
+})
+class GeneralOrderGeneratorTest {
+
+    private static final String BEARER_TOKEN = "Bearer Token";
+    private static final String REFERENCE_NUMBER = "000DC001";
+    private static final byte[] bytes = {1, 2, 3, 4, 5, 6};
+    private static final String fileName = format(GENERAL_ORDER.getDocumentTitle(), REFERENCE_NUMBER);
+    private static final CaseDocument CASE_DOCUMENT = CaseDocument.builder()
+        .documentName(fileName)
+        .documentType(DocumentType.GENERAL_ORDER)
+        .build();
+
+    @MockBean
+    private UnsecuredDocumentManagementService documentManagementService;
+    @MockBean
+    private DocumentGeneratorService documentGeneratorService;
+    @Autowired
+    private GeneralOrderGenerator generalOrderGenerator;
+
+    @Test
+    void shouldGenerateGeneralOrderDocument() {
+        CaseData caseData = CaseDataBuilder.builder().generalOrderApplication().build();
+
+        when(documentGeneratorService.generateDocmosisDocument(any(MappableObject.class), eq(GENERAL_ORDER)))
+            .thenReturn(new DocmosisDocument(GENERAL_ORDER.getDocumentTitle(), bytes));
+
+        when(documentManagementService.uploadDocument(
+            BEARER_TOKEN,
+            new PDF(fileName, bytes, DocumentType.GENERAL_ORDER)
+        ))
+            .thenReturn(CASE_DOCUMENT);
+
+        CaseDocument caseDocument = generalOrderGenerator.generate(caseData, BEARER_TOKEN);
+        assertThat(caseDocument).isNotNull().isEqualTo(CASE_DOCUMENT);
+
+        verify(documentManagementService).uploadDocument(
+            BEARER_TOKEN,
+            new PDF(fileName, bytes, DocumentType.GENERAL_ORDER)
+        );
+        verify(documentGeneratorService).generateDocmosisDocument(any(GeneralOrder.class), eq(GENERAL_ORDER));
+    }
+
+    @Nested
+    class GetTemplateData {
+
+        @Test
+        void whenJudgeMakeDecision_ShouldGetGeneralOrderData() {
+            CaseData caseData = CaseDataBuilder.builder().generalOrderApplication().build().toBuilder()
+                .build();
+
+            var templateData = generalOrderGenerator.getTemplateData(caseData);
+
+            assertThatFieldsAreCorrect_GeneralOrder(templateData, caseData);
+        }
+
+        private void assertThatFieldsAreCorrect_GeneralOrder(GeneralOrder templateData, CaseData caseData) {
+            Assertions.assertAll(
+                "GeneralOrderDocument data should be as expected",
+                () -> assertEquals(templateData.getClaimNumber(), caseData.getCcdCaseReference().toString()),
+                () -> assertEquals(templateData.getClaimantName(), getClaimats(caseData)),
+                () -> assertEquals(templateData.getDefendantName(), getDefendats(caseData)),
+                () -> assertEquals(templateData.getApplicationType(), getApplicationType(caseData)),
+                () -> assertEquals(templateData.getSubmittedOn(), caseData.getSubmittedOn()),
+                () -> assertEquals(templateData.getApplicantName(), caseData.getApplicantPartyName()),
+                () -> assertEquals(templateData.getApplicationDate(), caseData.getCreatedDate().toLocalDate()),
+                () -> assertEquals(templateData.getGeneralOrder(),
+                                   caseData.getJudicialDecisionMakeOrder().getOrderText()),
+                () -> assertEquals(templateData.getReasonForDecision(),
+                                   caseData.getJudicialDecisionMakeOrder().getReasonForDecisionText())
+            );
+        }
+
+        private String getClaimats(CaseData caseData) {
+            List<String> claimantsName = new ArrayList<>();
+            claimantsName.add(caseData.getClaimant1PartyName());
+            if (caseData.getDefendant2PartyName() != null) {
+                claimantsName.add(caseData.getClaimant2PartyName());
+            }
+            return String.join(", ", claimantsName);
+        }
+
+        private String getDefendats(CaseData caseData) {
+            List<String> defendatsName = new ArrayList<>();
+            defendatsName.add(caseData.getDefendant1PartyName());
+            if (caseData.getDefendant2PartyName() != null) {
+                defendatsName.add(caseData.getDefendant2PartyName());
+            }
+            return String.join(", ", defendatsName);
+        }
+
+        private String getApplicationType(CaseData caseData) {
+            List<GeneralApplicationTypes> types = caseData.getGeneralAppType().getTypes();
+            return types.stream()
+                .map(GeneralApplicationTypes::getDisplayedValue).collect(Collectors.joining(", "));
+        }
+    }
+}
