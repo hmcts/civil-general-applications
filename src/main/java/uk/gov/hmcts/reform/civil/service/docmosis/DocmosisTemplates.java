4c4c975b68334c6aa341ab8bcd171a881b627ac0
Maciej Nycz <maciej.nycz@version1.com>
2022-05-12 10:24:20 +0100

    fix checkstyle

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
@@ -6,8 +6,8 @@ public enum DocmosisTemplates {
     DISMISSAL_ORDER("DismissalOrder.docx", "Dismissal_order.pdf"),
     REQUEST_FOR_INFORMATION("Requestforinformation.docx", "Request_for_information.pdf"),
     HEARING_ORDER("HearingOrder.docx", "Hearing_order.pdf"),
-    WRITTEN_REP_SEQUENTIAL("OrderWrittenReps.docx", "Order_Written_Representation_Sequential.pdf"),
-    WRITTEN_REP_CONCURRENT("OrderWrittenRepsConcurrent.docx", "Order_Written_Representation_Concurrent.pdf");
+    WRITTEN_REPRESENTATION_SEQUENTIAL("OrderWrittenReps.docx", "Order_Written_Representation_Sequential.pdf"),
+    WRITTEN_REPRESENTATION_CONCURRENT("OrderWrittenRepsConcurrent.docx", "Order_Written_Representation_Concurrent.pdf");
 
     private final String template;
     private final String documentTitle;
