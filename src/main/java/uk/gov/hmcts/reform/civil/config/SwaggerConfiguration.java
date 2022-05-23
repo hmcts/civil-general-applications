934e645772d75a60e51eeb4177772469dcbc00c5
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-05-23 20:53:36 +0100

    Update SwaggerConfiguration.java

--- src/main/java/uk/gov/hmcts/reform/civil/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/SwaggerConfiguration.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.civil.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
