4c9bbffbb47267b86141ca38d6b2a535004fd1f8
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-06-01 09:51:28 +0100

    CIV-1428 Add proper tags to class JudicialDecisionHelper

--- src/main/java/uk/gov/hmcts/reform/civil/service/JudicialDecisionHelper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/JudicialDecisionHelper.java
@@ -1,11 +1,17 @@
 package uk.gov.hmcts.reform.civil.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class JudicialDecisionHelper {
 
     public YesOrNo isApplicationCloaked(CaseData caseData) {
