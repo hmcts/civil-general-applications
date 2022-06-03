4fdca51d3005d8b011a90ec02361a79dc010448a
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-06-03 14:27:55 +0100

    added dismissed status when the claim has been dismissed

--- src/main/java/uk/gov/hmcts/reform/civil/service/StateGeneratorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/StateGeneratorService.java
@@ -3,15 +3,15 @@ package uk.gov.hmcts.reform.civil.service;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption;
+import uk.gov.hmcts.reform.civil.enums.dq.GAJudgeMakeAnOrderOption;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import static org.apache.commons.lang.StringUtils.isBlank;
-import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_ADDITIONAL_INFORMATION;
-import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_DIRECTIONS_ORDER_DOCS;
-import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_WRITTEN_REPRESENTATIONS;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.*;
 import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.MAKE_AN_ORDER;
 import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.MAKE_ORDER_FOR_WRITTEN_REPRESENTATIONS;
 import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.REQUEST_MORE_INFO;
+import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeMakeAnOrderOption.DISMISS_THE_APPLICATION;
 
 @Service
 public class StateGeneratorService {
@@ -19,9 +19,10 @@ public class StateGeneratorService {
     public CaseState getCaseStateForEndJudgeBusinessProcess(CaseData data) {
         GAJudgeDecisionOption decision;
         String directionsText;
-
+        Boolean hasDismissedCase = false;
         if (data.getJudicialDecisionMakeOrder() != null) {
             directionsText = data.getJudicialDecisionMakeOrder().getDirectionsText();
+            hasDismissedCase = data.getJudicialDecisionMakeOrder().getMakeAnOrder().getDisplayedValue() == DISMISS_THE_APPLICATION.getDisplayedValue() ? true : false;
         } else {
             directionsText = null;
         }
@@ -31,13 +32,13 @@ public class StateGeneratorService {
             decision = null;
         }
 
-        if (decision == MAKE_AN_ORDER && !isBlank(directionsText)) {
+        if (hasDismissedCase ){
+            return APPLICATION_DISMISSED;
+        } else if (decision == MAKE_AN_ORDER && !isBlank(directionsText)) {
             return AWAITING_DIRECTIONS_ORDER_DOCS;
-        }
-        if (decision == REQUEST_MORE_INFO) {
+        } else if (decision == REQUEST_MORE_INFO) {
             return AWAITING_ADDITIONAL_INFORMATION;
-        }
-        if (decision == MAKE_ORDER_FOR_WRITTEN_REPRESENTATIONS) {
+        } else if (decision == MAKE_ORDER_FOR_WRITTEN_REPRESENTATIONS) {
             return AWAITING_WRITTEN_REPRESENTATIONS;
         }
         return data.getCcdState();
