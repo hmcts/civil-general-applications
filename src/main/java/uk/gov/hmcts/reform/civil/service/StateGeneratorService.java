1b7a824503171409f6ef51a619f62f68d4e8b8ba
Leonardo Palmeiro <leonardo.palmeiro@hmcts.net>
2022-06-06 08:54:08 +0100

    fixed code styles

--- src/main/java/uk/gov/hmcts/reform/civil/service/StateGeneratorService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/StateGeneratorService.java
@@ -3,14 +3,16 @@ package uk.gov.hmcts.reform.civil.service;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.enums.CaseState;
 import uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption;
-import uk.gov.hmcts.reform.civil.enums.dq.GAJudgeMakeAnOrderOption;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 
 import static org.apache.commons.lang.StringUtils.isBlank;
-import static uk.gov.hmcts.reform.civil.enums.CaseState.*;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.APPLICATION_DISMISSED;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_DIRECTIONS_ORDER_DOCS;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_ADDITIONAL_INFORMATION;
+import static uk.gov.hmcts.reform.civil.enums.CaseState.AWAITING_WRITTEN_REPRESENTATIONS;
 import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.MAKE_AN_ORDER;
-import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.MAKE_ORDER_FOR_WRITTEN_REPRESENTATIONS;
 import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.REQUEST_MORE_INFO;
+import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeDecisionOption.MAKE_ORDER_FOR_WRITTEN_REPRESENTATIONS;
 import static uk.gov.hmcts.reform.civil.enums.dq.GAJudgeMakeAnOrderOption.DISMISS_THE_APPLICATION;
 
 @Service
@@ -19,10 +21,13 @@ public class StateGeneratorService {
     public CaseState getCaseStateForEndJudgeBusinessProcess(CaseData data) {
         GAJudgeDecisionOption decision;
         String directionsText;
-        Boolean hasDismissedCase = false;
+        Boolean hasDismissedCase = Boolean.FALSE;
         if (data.getJudicialDecisionMakeOrder() != null) {
             directionsText = data.getJudicialDecisionMakeOrder().getDirectionsText();
-            hasDismissedCase = data.getJudicialDecisionMakeOrder().getMakeAnOrder().getDisplayedValue() == DISMISS_THE_APPLICATION.getDisplayedValue() ? true : false;
+            hasDismissedCase = data
+                .getJudicialDecisionMakeOrder()
+                .getMakeAnOrder()
+                .getDisplayedValue().equals(DISMISS_THE_APPLICATION.getDisplayedValue()) ? Boolean.TRUE : Boolean.FALSE;
         } else {
             directionsText = null;
         }
@@ -32,7 +37,7 @@ public class StateGeneratorService {
             decision = null;
         }
 
-        if (hasDismissedCase ){
+        if (Boolean.TRUE.equals(hasDismissedCase)) {
             return APPLICATION_DISMISSED;
         } else if (decision == MAKE_AN_ORDER && !isBlank(directionsText)) {
             return AWAITING_DIRECTIONS_ORDER_DOCS;
