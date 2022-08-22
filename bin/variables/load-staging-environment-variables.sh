0c487abdf9cb53b89d77fb3685a275297db69fb3
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-08-22 11:24:21 +0100

    CIV-4462 update SERVICE_AUTH_PROVIDER_API_BASE_URL

--- bin/variables/load-staging-environment-variables.sh
+++ bin/variables/load-staging-environment-variables.sh
@@ -5,7 +5,7 @@ set -eu
 echo 'export ENVIRONMENT=aat'
 
 # urls
-echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=http://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
+echo "export SERVICE_AUTH_PROVIDER_API_BASE_URL=https://rpe-service-auth-provider-aat.service.core-compute-aat.internal"
 echo "export IDAM_API_BASE_URL=https://idam-api.aat.platform.hmcts.net"
 echo "export CCD_IDAM_REDIRECT_URL=https://ccd-case-management-web-aat.service.core-compute-aat.internal/oauth2redirect"
 echo "export CCD_DEFINITION_STORE_API_BASE_URL=https://civil-ga-definition-store-staging-aat.service.core-compute-aat.internal"
