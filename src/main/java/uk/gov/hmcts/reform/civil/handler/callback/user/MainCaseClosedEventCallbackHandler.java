c466a97548c1fe35a1a77bd242818013808bd3df
smita.talawadekar <smita.talawadekar@HMCTS.NET>
2022-09-12 21:41:19 +0100

    CIV-2801 Changes as per latest requirement change

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/MainCaseClosedEventCallbackHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/MainCaseClosedEventCallbackHandler.java
@@ -24,7 +24,6 @@ import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.MAIN_CASE_CLOSED;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.APPLICATION_CLOSED;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.APPLICATION_DISMISSED;
-import static uk.gov.hmcts.reform.civil.enums.CaseState.LISTING_FOR_A_HEARING;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.ORDER_MADE;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.PROCEEDS_IN_HERITAGE;
 
