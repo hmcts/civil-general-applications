777e46d1f2196e939b3f5524693174d9482a9aec
MMNycz <maciej.nycz@version1.com>
2022-09-15 12:08:43 +0100

    CIV-4292 UpdateTest
    CIV-4292 UpdateTest

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/payment/PaymentsCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/payment/PaymentsCallbackHandlerTest.java
@@ -213,7 +213,8 @@ class PaymentsCallbackHandlerTest extends BaseCallbackHandlerTest {
             status,
             "exception message",
             Request.create(GET, "", Map.of(), new byte[]{}, UTF_8, null),
-            body
+            body,
+            Map.of()
         );
     }
 }
