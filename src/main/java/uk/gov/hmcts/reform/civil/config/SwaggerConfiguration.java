041a99cf6a7b3942895d3082f1c96f017dc00fca
MMNycz <maciej.nycz@version1.com>
2022-09-15 14:29:45 +0100

    Update SwaggerConfiguration.java

--- src/main/java/uk/gov/hmcts/reform/civil/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/SwaggerConfiguration.java
@@ -2,12 +2,14 @@ package uk.gov.hmcts.reform.civil.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import uk.gov.hmcts.reform.civil.Application;
 
+@EnableWebMvc
 @Configuration
 public class SwaggerConfiguration {
 
