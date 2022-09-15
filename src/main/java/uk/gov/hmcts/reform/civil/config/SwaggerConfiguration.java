342f16920477d5af1a4b9e2ae951a15c578ff435
MMNycz <maciej.nycz@version1.com>
2022-09-15 17:05:53 +0100

    Update springfoxSwagger to 3.0.0

--- src/main/java/uk/gov/hmcts/reform/civil/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/civil/config/SwaggerConfiguration.java
@@ -2,14 +2,14 @@ package uk.gov.hmcts.reform.civil.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.civil.Application;
 
-@EnableSwagger2
+@EnableWebMvc
 @Configuration
 public class SwaggerConfiguration {
 
