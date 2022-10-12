eb07e6baf3678f46c5af8b61e928a3cc36f2ab19
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2022-10-12 10:57:17 +0100

    Revert "CIV-5085 fix update ga status with visible to resp sol; add test cases (#585)"
    This reverts commit 4263b335bc75dbb5133f1755e230aab93b14a917.

--- src/test/java/uk/gov/hmcts/reform/civil/service/ParentCaseUpdateHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/ParentCaseUpdateHelperTest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.civil.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,15 +10,8 @@ import uk.gov.hmcts.reform.ccd.client.model.StartEventResponse;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.CaseLink;
-import uk.gov.hmcts.reform.civil.model.common.Element;
-import uk.gov.hmcts.reform.civil.model.genapplication.GADetailsRespondentSol;
-import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
-import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplicationsDetails;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDetailsBuilder;
-
-import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
@@ -30,8 +22,6 @@ import static uk.gov.hmcts.reform.civil.enums.CaseState.APPLICATION_ADD_PAYMENT;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.ORDER_MADE;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
-import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
-import static uk.gov.hmcts.reform.civil.utils.ElementUtils.wrapElements;
 
 @SpringBootTest(classes = {
     ParentCaseUpdateHelper.class,
@@ -60,28 +50,6 @@ class ParentCaseUpdateHelperTest {
         verify(coreCaseDataService, times(1)).submitUpdate(any(), any());
     }
 
-    @Test
-    void updateParentApplicationVisibilityWithNewStateWithoutRespondentSol() {
-        CaseData caseData = getCaseWithApplicationData(false);
-        when(coreCaseDataService.startUpdate(any(), any())).thenReturn(getStartEventResponse(YES, NO));
-        when(caseDetailsConverter.toCaseData(any())).thenReturn(caseData);
-
-        parentCaseUpdateHelper.updateParentApplicationVisibilityWithNewState(caseData, ORDER_MADE.toString());
-        verify(coreCaseDataService, times(1)).submitUpdate(any(), any());
-
-    }
-
-    @Test
-    void updateParentApplicationVisibilityWithNewStateWithRespondentSol() {
-        CaseData caseData = getCaseWithApplicationData(true);
-        when(coreCaseDataService.startUpdate(any(), any())).thenReturn(getStartEventResponse(YES, NO));
-        when(caseDetailsConverter.toCaseData(any())).thenReturn(caseData);
-
-        parentCaseUpdateHelper.updateParentApplicationVisibilityWithNewState(caseData, ORDER_MADE.toString());
-        verify(coreCaseDataService, times(1)).submitUpdate(any(), any());
-
-    }
-
     private StartEventResponse getStartEventResponse(YesOrNo isConsented, YesOrNo isTobeNotified) {
         CaseDetails caseDetails = CaseDetailsBuilder.builder().data(
             CaseDataBuilder.builder().judicialOrderMadeWithUncloakApplication(NO).build())
@@ -94,37 +62,6 @@ class ParentCaseUpdateHelperTest {
             .caseDetails(caseDetails);
 
         return startEventResponseBuilder.build();
-    }
-
-    private CaseData getCaseWithApplicationData(Boolean withRespondentSol) {
-        GeneralApplication generalApplication = GeneralApplication
-                .builder()
-                .caseLink(CaseLink.builder().caseReference(CaseDataBuilder.CASE_ID.toString()).build())
-                .build();
-        List<Element<GeneralApplication>> generalApplications = wrapElements(generalApplication);
-
-        List<Element<GeneralApplicationsDetails>> generalApplicationsDetailsList = Lists.newArrayList();
-        List<Element<GADetailsRespondentSol>> gaDetailsRespondentSolList = Lists.newArrayList();
-
-        GeneralApplicationsDetails generalApplicationsDetails = GeneralApplicationsDetails.builder()
-                .generalApplicationType("Summary judgment")
-                .generalAppSubmittedDateGAspec(generalApplication.getGeneralAppSubmittedDateGAspec())
-                .caseLink(generalApplication.getCaseLink())
-                .caseState("pending").build();
-        generalApplicationsDetailsList.add(element(generalApplicationsDetails));
-
-        GADetailsRespondentSol gaDetailsRespondentSol = GADetailsRespondentSol.builder()
-                .generalApplicationType("Summary judgment")
-                .generalAppSubmittedDateGAspec(generalApplication.getGeneralAppSubmittedDateGAspec())
-                .caseLink(generalApplication.getCaseLink())
-                .caseState("pending").build();
-        gaDetailsRespondentSolList.add(element(gaDetailsRespondentSol));
-
-        return CaseDataBuilder.builder().judicialOrderMadeWithUncloakApplication(NO)
-                .generalApplications(generalApplications)
-                .generalApplicationsDetails(generalApplicationsDetailsList)
-                .gaDetailsRespondentSol(withRespondentSol ? gaDetailsRespondentSolList : null)
-                .submittedOn(null).build();
     }
 
 }
