3327cf7fbadde4b6ae0e5b11cf925ba6b006a9c5
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2022-12-02 11:18:34 +0000

    CIV-5443 refactor judgement to judgment

--- src/main/java/uk/gov/hmcts/reform/civil/enums/dq/GeneralApplicationTypes.java
+++ src/main/java/uk/gov/hmcts/reform/civil/enums/dq/GeneralApplicationTypes.java
@@ -12,12 +12,12 @@ public enum GeneralApplicationTypes {
     EXTEND_TIME("Extend time"),
     AMEND_A_STMT_OF_CASE("Amend a statement of case"),
     RELIEF_FROM_SANCTIONS("Relief from sanctions"),
-    SET_ASIDE_JUDGEMENT("Set aside judgement"),
+    SET_ASIDE_JUDGEMENT("Set aside judgment"),
     VARY_ORDER("Vary order"),
     ADJOURN_VACATE_HEARING("Adjourn or vacate a hearing"),
     UNLESS_ORDER("Unless order"),
     OTHER("Other"),
-    VARY_JUDGEMENT("Vary judgement");
+    VARY_JUDGEMENT("Vary judgment");
 
     private final String displayedValue;
 }
