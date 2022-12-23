fb123bdecb2bae7131972a063e5aeeba96322ed4
Pijian Liao <Pijian.Liao@HMCTS.NET>
2022-12-23 12:19:09 +0000

    CIV-6011 add event with empty handler

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/NotifyHearingHandler.java
@@ -0,0 +1,47 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.SCHEDULE_HEARING;
+
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NotifyHearingHandler extends CallbackHandler {
+    private static final List<CaseEvent> EVENTS = Collections.singletonList(SCHEDULE_HEARING);
+
+
+    @Override
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+                callbackKey(ABOUT_TO_START), this::nothing,
+                callbackKey(ABOUT_TO_SUBMIT), this::nothing,
+                callbackKey(SUBMITTED), this::nothing
+        );
+    }
+
+    private CallbackResponse nothing(CallbackParams callbackParams) {
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .build();
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+}
