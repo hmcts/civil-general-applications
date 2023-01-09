3957943190977b046c64f1a04ce28a7a3eb06341
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-01-09 16:27:46 +0000

    CIV-6052 screen 2

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/NotifyHearingHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/NotifyHearingHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
@@ -8,15 +9,24 @@ import uk.gov.hmcts.reform.civil.callback.Callback;
 import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.LocationRefData;
+import uk.gov.hmcts.reform.civil.model.common.DynamicList;
+import uk.gov.hmcts.reform.civil.service.GeneralAppLocationRefDataService;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.SUBMITTED;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.HEARING_SCHEDULED;
+import static uk.gov.hmcts.reform.civil.model.common.DynamicList.fromList;
 
 @Service
 @RequiredArgsConstructor
@@ -24,13 +34,42 @@ public class NotifyHearingHandler extends CallbackHandler {
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(HEARING_SCHEDULED);
 
+    private final GeneralAppLocationRefDataService locationRefDataService;
+
+    private final ObjectMapper objectMapper;
+
     @Override
     protected Map<String, Callback> callbacks() {
         return Map.of(
-                callbackKey(ABOUT_TO_START), this::nothing,
+                callbackKey(ABOUT_TO_START), this::emptyCallbackResponse,
+                callbackKey(MID, "locationName"), this::locationList,
                 callbackKey(ABOUT_TO_SUBMIT), this::nothing,
                 callbackKey(SUBMITTED), this::nothing
         );
+    }
+
+    private CallbackResponse locationList(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
+
+        String authToken = callbackParams.getParams().get(BEARER_TOKEN).toString();
+        DynamicList dynamicLocationList = getLocationsFromList(locationRefDataService.getCourtLocations(authToken));
+        if(Objects.nonNull(caseData)) {
+
+        }
+        caseDataBuilder.hearingLocation(dynamicLocationList)
+                .build();
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+                .data(caseDataBuilder.build().toMap(objectMapper))
+                .build();
+    }
+
+    private DynamicList getLocationsFromList(final List<LocationRefData> locations) {
+        return fromList(locations.stream().map(location -> new StringBuilder().append(location.getSiteName())
+                        .append(" - ").append(location.getCourtAddress())
+                        .append(" - ").append(location.getPostcode()).toString())
+                .collect(Collectors.toList()));
     }
 
     private CallbackResponse nothing(CallbackParams callbackParams) {
