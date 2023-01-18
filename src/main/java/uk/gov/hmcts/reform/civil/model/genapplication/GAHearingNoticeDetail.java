741108f52a047a3f544a8e07fcbf91b2673c2879
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-01-18 17:06:57 +0000

    CIV-6823 refactor enum

--- src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/GAHearingNoticeDetail.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/genapplication/GAHearingNoticeDetail.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
-import uk.gov.hmcts.reform.civil.enums.hearing.HearingChannel;
-import uk.gov.hmcts.reform.civil.enums.hearing.HearingDuration;
+
+import uk.gov.hmcts.reform.civil.enums.GAJudicialHearingType;
+import uk.gov.hmcts.reform.civil.enums.dq.GAHearingDuration;
 import uk.gov.hmcts.reform.civil.model.common.DynamicList;
 
 import java.time.LocalDate;
@@ -19,16 +20,16 @@ public class GAHearingNoticeDetail {
     private DynamicList hearingLocation;
     private LocalDate hearingDate;
     private String hearingTimeHourMinute;
-    private HearingChannel channel;
-    private HearingDuration hearingDuration;
+    private GAJudicialHearingType channel;
+    private GAHearingDuration hearingDuration;
     private String hearingDurationOther;
 
     @JsonCreator
     GAHearingNoticeDetail(@JsonProperty("hearingLocation") DynamicList hearingLocation,
                           @JsonProperty("hearingDate") LocalDate hearingDate,
                           @JsonProperty("hearingTimeHourMinute") String hearingTimeHourMinute,
-                          @JsonProperty("channel") HearingChannel channel,
-                          @JsonProperty("hearingDuration") HearingDuration hearingDuration,
+                          @JsonProperty("channel") GAJudicialHearingType channel,
+                          @JsonProperty("hearingDuration") GAHearingDuration hearingDuration,
                           @JsonProperty("hearingDurationOther") String hearingDurationOther) {
         this.hearingLocation = hearingLocation;
         this.hearingDate = hearingDate;
