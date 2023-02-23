6d869b60bce35453667e77523da2e52ade61dbe6
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2023-02-23 18:06:38 +0000

    CIV-6861 remove condition expression

--- src/main/java/uk/gov/hmcts/reform/civil/service/tasklisteners/GAUnlessOrderSchedulerExternalTaskListener.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/tasklisteners/GAUnlessOrderSchedulerExternalTaskListener.java
@@ -3,12 +3,10 @@ package uk.gov.hmcts.reform.civil.service.tasklisteners;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.civil.handler.tasks.CheckUnlessOrderDeadlineEndTaskHandler;
 
 @Component
-@ConditionalOnExpression("${response.deadline.check.event.emitter.enabled:true}")
 public class GAUnlessOrderSchedulerExternalTaskListener {
 
     private static final String TOPIC = "GAUnlessOrderScheduler";
