ed382a1556caade5ab79a553ceb29cc01ad8ef4b
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2023-02-23 12:01:27 +0000

    CIV-6861 Fix db query

--- src/main/java/uk/gov/hmcts/reform/civil/service/search/CaseStateSearchService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/search/CaseStateSearchService.java
@@ -11,6 +11,7 @@ import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 
 import static java.util.Collections.emptyList;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static uk.gov.hmcts.reform.civil.enums.dq.GeneralApplicationTypes.UNLESS_ORDER;
 
 @Service
 public class CaseStateSearchService extends ElasticSearchService {
@@ -33,7 +34,10 @@ public class CaseStateSearchService extends ElasticSearchService {
     Query queryForOrderMade(int startIndex, CaseState caseState, GeneralApplicationTypes gaType) {
         MatchQueryBuilder queryCaseState = QueryBuilders.matchQuery("state", caseState.toString());
         MatchQueryBuilder queryGaType = QueryBuilders.matchQuery("data.generalAppType.types", gaType);
-        MatchQueryBuilder queryOrderProcessStatus = QueryBuilders
+        MatchQueryBuilder queryOrderProcessStatus = gaType.equals(UNLESS_ORDER)
+            ? QueryBuilders
+            .matchQuery("data.judicialDecisionMakeOrder.isOrderProcessedByUnlessScheduler", "No")
+            : QueryBuilders
             .matchQuery("data.judicialDecisionMakeOrder.isOrderProcessedByStayScheduler", "No");
 
         BoolQueryBuilder query = QueryBuilders.boolQuery();
