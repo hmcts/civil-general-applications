e817212043feb00de9074f37e39453cee66e4ed2
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-03-13 17:19:41 +0000

    CIV-7535 add tests

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandlerTest.java
@@ -1,20 +1,53 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GENERATE_DIRECTIONS_ORDER;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.INITIATE_GENERAL_APPLICATION;
 
 @SpringBootTest(classes = {
-        JudicialFinalDecisionHandlerTest.class,
+        JudicialFinalDecisionHandler.class,
         JacksonAutoConfiguration.class,
 })
-class JudicialFinalDecisionHandlerTest {
+class JudicialFinalDecisionHandlerTest extends BaseCallbackHandlerTest {
+
+    @Autowired
+    private JudicialFinalDecisionHandler handler;
+    @Autowired
+    private ObjectMapper objMapper;
+
+    @Test
+    void handleEventsReturnsTheExpectedCallbackEvent() {
+        assertThat(handler.handledEvents()).contains(GENERATE_DIRECTIONS_ORDER);
+    }
+
+    @Test
+    void setCaseName() {
+        CaseData caseData = CaseDataBuilder.builder()
+                .atStateClaimDraft()
+                .build().toBuilder()
+                .claimant1PartyName("Mr. John Rambo")
+                .defendant1PartyName("Mr. Sole Trader")
+                .build();
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_START);
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        assertThat(response.getData().get("caseNameHmctsInternal")
+                .toString()).isEqualTo("Mr. John Rambo V Mr. Sole Trader");
+    }
 
     @Nested
     class GetAllPartyNames {
