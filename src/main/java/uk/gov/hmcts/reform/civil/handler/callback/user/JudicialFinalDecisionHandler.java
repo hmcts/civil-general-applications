0172d5c49340af28797d7a424e871751457545d2
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-03-13 16:37:46 +0000

    CIV-7535 unit test

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandler.java
@@ -1,13 +1,5 @@
 package uk.gov.hmcts.reform.civil.handler.callback.user;
 
-import static java.lang.String.format;
-import static uk.gov.hmcts.reform.civil.callback.CallbackParams.Params.BEARER_TOKEN;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
-import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
-import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GENERATE_DIRECTIONS_ORDER;
-import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
-import static uk.gov.hmcts.reform.civil.enums.YesOrNo.YES;
-
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
 import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
 import uk.gov.hmcts.reform.civil.callback.Callback;
@@ -15,20 +7,21 @@ import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.common.DynamicList;
-import uk.gov.hmcts.reform.civil.model.common.DynamicListElement;
-import uk.gov.hmcts.reform.civil.model.genapplication.GAHearingNoticeDetail;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import static java.lang.String.format;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GENERATE_DIRECTIONS_ORDER;
+import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 
 @Slf4j
 @Service
@@ -61,10 +54,8 @@ public class JudicialFinalDecisionHandler extends CallbackHandler {
     }
 
     public static String getAllPartyNames(CaseData caseData) {
-        return format("%s%s V %s%s",
+        return format("%s V %s%s",
                 caseData.getClaimant1PartyName(),
-                YES.equals(caseData.getAddApplicant2())
-                        ? ", " + caseData.getClaimant2PartyName() : "",
                 caseData.getDefendant1PartyName(),
                 Objects.nonNull(caseData.getDefendant2PartyName())
                         && NO.equals(caseData.getRespondent2SameLegalRepresentative())
---
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandlerTest.java
@@ -0,0 +1,69 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {
+        JudicialFinalDecisionHandlerTest.class,
+        JacksonAutoConfiguration.class,
+})
+class JudicialFinalDecisionHandlerTest {
+
+//    @Autowired
+//    private JudicialFinalDecisionHandler handler;
+//    @Autowired
+//    private ObjectMapper objMapper;
+
+    @Nested
+    class GetAllPartyNames {
+        @Test
+        void oneVOne() {
+            CaseData caseData = CaseDataBuilder.builder()
+                    .atStateClaimDraft()
+                    .build().toBuilder()
+                    .claimant1PartyName("Mr. John Rambo")
+                    .defendant1PartyName("Mr. Sole Trader")
+                    .build();
+            String title = JudicialFinalDecisionHandler.getAllPartyNames(caseData);
+            assertThat(title).isEqualTo("Mr. John Rambo V Mr. Sole Trader");
+        }
+
+        @Test
+        void oneVTwoSameSol() {
+            CaseData caseData = CaseDataBuilder.builder()
+                    .atStateClaimDraft()
+                    .build().toBuilder()
+                    .respondent2SameLegalRepresentative(YesOrNo.YES)
+                    .claimant1PartyName("Mr. John Rambo")
+                    .defendant1PartyName("Mr. Sole Trader")
+                    .defendant2PartyName("Mr. John Rambo")
+                    .build();
+
+            String title = JudicialFinalDecisionHandler.getAllPartyNames(caseData);
+            assertThat(title).isEqualTo("Mr. John Rambo V Mr. Sole Trader");
+        }
+
+        @Test
+        void oneVTwo() {
+            CaseData caseData = CaseDataBuilder.builder()
+                    .atStateClaimDraft()
+                    .build().toBuilder()
+                    .respondent2SameLegalRepresentative(YesOrNo.NO)
+                    .claimant1PartyName("Mr. John Rambo")
+                    .defendant1PartyName("Mr. Sole Trader")
+                    .defendant2PartyName("Mr. John Rambo")
+                    .build();
+
+            String title = JudicialFinalDecisionHandler.getAllPartyNames(caseData);
+            assertThat(title).isEqualTo("Mr. John Rambo V Mr. Sole Trader, Mr. John Rambo");
+        }
+    }
+}
