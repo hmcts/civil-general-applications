9e9c34566238da7a5367695e634b8410df812061
Deepthi Doppalapudi <deepthi.doppalapudi@version1.com>
2023-03-15 17:51:41 +0000

    CIV-4995 Added new handler for updating ga location after SDO

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/UpdateGaLocationCallbackHandler.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.civil.handler.callback.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CallbackResponse;
+import uk.gov.hmcts.reform.civil.callback.Callback;
+import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
+import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.genapplication.GACaseLocation;
+import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
+
+import java.util.List;
+import java.util.Map;
+
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.TRIGGER_LOCATION_UPDATE;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UpdateGaLocationCallbackHandler extends CallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = List.of(TRIGGER_LOCATION_UPDATE);
+    private final CoreCaseDataService coreCaseDataService;
+    private final CaseDetailsConverter caseDetailsConverter;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    protected Map<String, Callback> callbacks() {
+        return Map.of(
+            callbackKey(ABOUT_TO_SUBMIT), this::updateLocation
+        );
+    }
+
+    private CallbackResponse updateLocation(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData civilCaseData = caseDetailsConverter
+            .toCaseData(coreCaseDataService
+                            .getCase(Long.parseLong(caseData.getGeneralAppParentCaseLink().getCaseReference())));
+        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
+        caseDataBuilder.caseManagementLocation(
+            GACaseLocation.builder().baseLocation(civilCaseData.getCaseManagementLocation().getBaseLocation())
+                    .region(civilCaseData.getCaseManagementLocation().getRegion()).build());
+        caseDataBuilder.isCcmccLocation(YesOrNo.NO);
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+            .data(caseDataBuilder.build().toMap(objectMapper))
+            .build();
+    }
+}
