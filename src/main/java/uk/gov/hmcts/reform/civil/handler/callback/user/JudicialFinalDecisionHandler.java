61ea864952f06f99dd30bee4cb53432149c250aa
pliao-hmcts <113367232+pliao-hmcts@users.noreply.github.com>
2023-03-21 13:04:26 +0000

    CIV-7536 Journey 2 (#946)
    * CIV-7535 add event and case name
    
    * CIV-7535 unit test
    
    * CIV-7535 checkstyle
    
    * CIV-7535 add tests
    
    * CIV-7535 checkstyle
    
    * CIV-7536 free form
    
    * CIV-7535 fix 1v2 title
    
    * CIV-7535 checkstyle
    
    * CIV-7536 bug fix
    
    * CIV-7536 bug fix
    
    ---------
    
    Co-authored-by: Azam <106387766+Azam-Hmcts@users.noreply.github.com>
    Co-authored-by: Madhan Mahadevan <madhan.mahadevan@hmcts.net>

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/JudicialFinalDecisionHandler.java
@@ -11,7 +11,9 @@ import uk.gov.hmcts.reform.civil.callback.CallbackHandler;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
 import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.genapplication.FreeFormOrderValues;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +22,7 @@ import java.util.Objects;
 import static java.lang.String.format;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_START;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
+import static uk.gov.hmcts.reform.civil.callback.CallbackType.MID;
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.GENERATE_DIRECTIONS_ORDER;
 import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 
@@ -29,12 +32,19 @@ import static uk.gov.hmcts.reform.civil.enums.YesOrNo.NO;
 public class JudicialFinalDecisionHandler extends CallbackHandler {
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(GENERATE_DIRECTIONS_ORDER);
+    private static final String ON_INITIATIVE_SELECTION_TEST = "As this order was made on the court's own initiative "
+            + "any party affected by the order may apply to set aside, vary or stay the order."
+            + " Any such application must be made by 4pm on";
+    private static final String WITHOUT_NOTICE_SELECTION_TEXT = "If you were not notified of the application before "
+            + "this order was made, you may apply to set aside, vary or stay the order."
+            + " Any such application must be made by 4pm on";
     private final ObjectMapper objectMapper;
 
     @Override
     protected Map<String, Callback> callbacks() {
         return Map.of(
                 callbackKey(ABOUT_TO_START), this::setCaseName,
+                callbackKey(MID, "populate-freeForm-values"), this::populateFreeFormValues,
                 callbackKey(ABOUT_TO_SUBMIT), this::emptyCallbackResponse
         );
     }
@@ -48,13 +58,32 @@ public class JudicialFinalDecisionHandler extends CallbackHandler {
                 .build();
     }
 
+    public CallbackResponse populateFreeFormValues(CallbackParams callbackParams) {
+        CaseData caseData = callbackParams.getCaseData();
+        CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
+
+        caseDataBuilder.orderOnCourtInitiative(FreeFormOrderValues.builder()
+                .onInitiativeSelectionTextArea(ON_INITIATIVE_SELECTION_TEST)
+                .onInitiativeSelectionDate(LocalDate.now())
+                .build());
+        caseDataBuilder.orderWithoutNotice(FreeFormOrderValues.builder()
+                .withoutNoticeSelectionTextArea(WITHOUT_NOTICE_SELECTION_TEXT)
+                .withoutNoticeSelectionDate(LocalDate.now())
+                .build());
+        caseDataBuilder.freeFormOrderedText(caseData.getGeneralAppDetailsOfOrder());
+
+        return AboutToStartOrSubmitCallbackResponse.builder()
+                .data(caseDataBuilder.build().toMap(objectMapper))
+                .build();
+    }
+
     @Override
     public List<CaseEvent> handledEvents() {
         return EVENTS;
     }
 
     public static String getAllPartyNames(CaseData caseData) {
-        return format("%s V %s%s",
+        return format("%s v %s%s",
                 caseData.getClaimant1PartyName(),
                 caseData.getDefendant1PartyName(),
                 Objects.nonNull(caseData.getDefendant2PartyName())
