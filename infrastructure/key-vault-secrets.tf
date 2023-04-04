3d7f1edcbe293e3a01dd6cfe363546b7ac328b0e
Your Name <james.ferrari@version1.com>
2023-04-04 15:28:58 +0100

    add connection string to new kv

--- infrastructure/key-vault-secrets.tf
+++ infrastructure/key-vault-secrets.tf
@@ -7,3 +7,13 @@ resource "azurerm_key_vault_secret" "app-insights-instrumentation-key" {
     "source" : "app insights ${azurerm_application_insights.appinsights.name}"
   })
 }
+
+resource "azurerm_key_vault_secret" "app-insights-connection-string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.key-vault.key_vault_id
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "appinsights ${azurerm_application_insights.appinsights.name}"
+  })
+}
