3a73b8b2bea0b530e81b377e9754b36667b0a1e9
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-06-14 17:59:01 +0100

    CIV-8961 update main case

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/PollingEventEmitterHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/PollingEventEmitterHandler.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.civil.handler.tasks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.client.task.ExternalTask;
@@ -12,13 +13,19 @@ import uk.gov.hmcts.reform.civil.enums.BusinessProcessStatus;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.genapplication.GeneralApplication;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.EventEmitterService;
 import uk.gov.hmcts.reform.civil.service.search.CaseStateSearchService;
+import uk.gov.hmcts.reform.civil.utils.ElementUtils;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_BUSINESS_PROCESS_STATE;
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.UPDATE_CASE_WITH_GA_STATE;
 import static uk.gov.hmcts.reform.civil.utils.TaskHandlerUtil.gaCaseDataContent;
 
 @Slf4j
@@ -31,6 +38,7 @@ public class PollingEventEmitterHandler implements BaseExternalTaskHandler {
     private final CaseDetailsConverter caseDetailsConverter;
     private final EventEmitterService eventEmitterService;
     private final CoreCaseDataService coreCaseDataService;
+    private final ObjectMapper mapper;
 
     @Override
     public void handleTask(ExternalTask externalTask) {
@@ -46,9 +54,51 @@ public class PollingEventEmitterHandler implements BaseExternalTaskHandler {
         log.info("Emitting {} camunda event for case through poller: {}",
                  caseData.getBusinessProcess().getCamundaEvent(),
                  caseData.getCcdCaseReference());
+        String parentCaseId = caseData.getGeneralAppParentCaseLink().getCaseReference();
+        StartEventResponse startEventResponse = coreCaseDataService
+                .startUpdate(parentCaseId, UPDATE_CASE_WITH_GA_STATE);
 
-        startGAEventToUpdateState(caseData);
-        eventEmitterService.emitBusinessProcessCamundaGAEvent(caseData, true);
+        CaseData civilCaseData = caseDetailsConverter.toCaseData(startEventResponse.getCaseDetails());
+        if( caseData.getBusinessProcess().getCamundaEvent().equals("INITIATE_GENERAL_APPLICATION") ) {
+            Element<GeneralApplication> applicationElement = null;
+            int idx = -1;
+            for (Element<GeneralApplication> element : civilCaseData.getGeneralApplications()) {
+                idx++;
+                if (findGeneralApplicationElement(element,
+                        caseData.getCcdCaseReference().toString())) {
+                    applicationElement = element;
+                    applicationElement = ElementUtils.element(applicationElement.getValue().toBuilder()
+                            .businessProcess(BusinessProcess.builder().status(BusinessProcessStatus.READY).build()).build());
+                }
+            }
+            Map<String, Object> output = civilCaseData.toMap(mapper);
+            civilCaseData.getGeneralApplications().remove(idx);
+            civilCaseData.getGeneralApplications().add(applicationElement);
+            output.put("generalApplications", civilCaseData.getGeneralApplications());
+            CaseDataContent caseDataContent = coreCaseDataService.caseDataContentFromStartEventResponse(
+                    startEventResponse, output);
+
+            civilCaseData = coreCaseDataService.submitUpdate(parentCaseId, caseDataContent);
+            eventEmitterService.emitBusinessProcessCamundaEvent(
+                    Long.parseLong(parentCaseId), applicationElement.getValue(), true);
+        } else {
+            startGAEventToUpdateState(caseData);
+            eventEmitterService.emitBusinessProcessCamundaGAEvent(caseData, true);
+        }
+    }
+
+    private boolean findGeneralApplicationElement(Element<GeneralApplication> applicationElement,
+                                                  String caseId) {
+        if (Objects.nonNull(applicationElement.getValue().getCaseLink())
+            && applicationElement.getValue().getCaseLink().getCaseReference().equals(caseId)) {
+            return true;
+        } else if (Objects.isNull(applicationElement.getValue().getCaseLink())
+                && Objects.nonNull(applicationElement.getValue().getBusinessProcess())
+            && applicationElement.getValue().getBusinessProcess()
+                .getStatus().equals(BusinessProcessStatus.FINISHED)) {
+            return true;
+        }
+        return false;
     }
 
     @Override
