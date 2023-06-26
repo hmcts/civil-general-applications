504975c085674463c35366582a596c32b87df2c6
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-06-26 12:33:54 +0100

    CIV-9298 merge

--- src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/ApplicationProcessCaseEventTaskHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/tasks/ApplicationProcessCaseEventTaskHandler.java
@@ -14,7 +14,6 @@ import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.helpers.TaskHandlerHelper;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
-import uk.gov.hmcts.reform.civil.model.GeneralAppParentCaseLink;
 import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.data.ExternalTaskInput;
 import uk.gov.hmcts.reform.civil.service.flowstate.StateFlowEngine;
