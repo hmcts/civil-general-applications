6d1b7b220fe99b8399d8b32b97d59166878223e5
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2023-09-11 14:16:24 +0100

    Squashed commit of the following:
    commit d68ca6bf9b6e2d36722516677ff0ecb3fbce0a75
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Wed Sep 6 17:56:25 2023 +0100
    
        modifications on sonar exclusions
    
        pointing to ga ccd master
        - modify application yaml
    
    commit 1e50f2698a186aea47517a7f7cab243148aa8b10
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Wed Sep 6 10:51:07 2023 +0100
    
        few changes and log statements
    
    commit 8a7160fb33ea6ea4f03bd97665b6bc448552b7de
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Tue Sep 5 18:26:25 2023 +0100
    
        few minor changes
    
    commit 67d2c8cf6a931810ca96cf2bbd3ba49c3a3305d9
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Tue Sep 5 17:50:00 2023 +0100
    
        Update PaymentRequestUpdateCallbackControllerTest.java
    
    commit d5482c317506b68476ca8131cf53dfb4caaa78bd
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Tue Sep 5 17:40:21 2023 +0100
    
        removing trailing whitespace
    
    commit adb21a70c0cb1628e1d2d15b8f87916479bf1352
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Tue Sep 5 17:36:17 2023 +0100
    
        modifying the approach taken for s2s
    
        - adding new controller test for verifying logic change
        - changes in authorisation service
    
    commit 72511c9ede5896726bc706520616883bf763747c
    Merge: ffe7a217 d2a2cac9
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Tue Sep 5 13:04:36 2023 +0100
    
        Merge branch 'master' into s2spayments
    
    commit ffe7a2175c155e5e2fa688ea67801d3ee08db7e2
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 16:54:11 2023 +0100
    
        add temp logging
    
    commit bc104dfe3be04e6b747a8452d53db1412df807f3
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 16:13:52 2023 +0100
    
        adding chart variables
    
    commit 67798b3dab3c40e26bb7725287d273995fa6c8eb
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 15:53:43 2023 +0100
    
        Update build.gradle
    
    commit c40e7e2cb8aa284613d0320963871e2a76d927fb
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 15:19:56 2023 +0100
    
        fixing tests
    
    commit e895e0cfdbe5ef95cd7289cd5132571d806de837
    Merge: 4d4d5cb3 2851c15c
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 15:05:50 2023 +0100
    
        Merge branch 's2spayments' of https://github.com/hmcts/civil-general-applications into s2spayments
    
    commit 4d4d5cb332d2f1f3f82c5cf52cbd3437a90b06c3
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 15:05:23 2023 +0100
    
        expanding controller test for coverage
    
    commit 2851c15ca7f92ddc0b0f584dbea2142907017ca2
    Author: hmcts-jenkins-a-to-c <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
    Date:   Fri Jul 14 13:46:52 2023 +0000
    
        Bumping chart version/ fixing aliases
    
    commit 8fa83ea44a5b40ec24ccc63d1d8a446b067072b8
    Author: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
    Date:   Fri Jul 14 14:45:53 2023 +0100
    
        initial commit with the relevant changes
    
        - introduced authorisation service
        - changes on payment call back service to check and throw relevant exception

--- src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/PaymentRequestUpdateCallbackControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/civil/controllers/PaymentRequestUpdateCallbackControllerTest.java
@@ -2,18 +2,21 @@ package uk.gov.hmcts.reform.civil.controllers;
 
 import jakarta.servlet.ServletException;
 import lombok.SneakyThrows;
-import org.apache.hc.core5.http.HttpHeaders;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import uk.gov.hmcts.reform.civil.model.ServiceRequestUpdateDto;
+import uk.gov.hmcts.reform.civil.service.PaymentRequestUpdateCallbackService;
 import uk.gov.hmcts.reform.payments.client.models.PaymentDto;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PaymentRequestUpdateCallbackControllerTest extends BaseIntegrationTest {
@@ -23,21 +26,39 @@ class PaymentRequestUpdateCallbackControllerTest extends BaseIntegrationTest {
     private static final String PAID = "Paid";
     private static final String REFERENCE = "reference";
     private static final String ACCOUNT_NUMBER = "123445555";
+    private static final String authToken = "Bearer TestAuthToken";
     private static final String s2sToken = "s2s AuthToken";
+
+    @MockBean
+    private PaymentRequestUpdateCallbackService requestUpdateCallbackService;
 
     @Test
     public void whenInvalidTypeOfRequestMade_ReturnMethodNotAllowed() throws Exception {
-
         doPost(buildServiceDto(), PAYMENT_CALLBACK_URL, "")
             .andExpect(status().isMethodNotAllowed());
     }
 
     @Test
     public void whenServiceRequestUpdateRequest() {
+        doThrow(new RuntimeException("Payment failure")).when(requestUpdateCallbackService).processCallback(buildServiceDto());
         Exception e = assertThrows(ServletException.class,
             () -> doPut(buildServiceDto(), PAYMENT_CALLBACK_URL, "")
         );
         assertThat(e.getMessage()).contains("PaymentException");
+    }
+
+    @Test
+    public void whenValidPaymentCallbackIsReceivedReturnSuccess() throws Exception {
+        doPut(buildServiceDto(), PAYMENT_CALLBACK_URL, "")
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    public void whenPaymentCallbackIsReceivedWithoutServiceAuthorisationReturn400() throws Exception {
+        mockMvc.perform(
+            MockMvcRequestBuilders.put(PAYMENT_CALLBACK_URL, "")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(buildServiceDto()))).andExpect(status().isBadRequest());
     }
 
     private ServiceRequestUpdateDto buildServiceDto() {
@@ -57,7 +78,6 @@ class PaymentRequestUpdateCallbackControllerTest extends BaseIntegrationTest {
     protected <T> ResultActions doPut(T content, String urlTemplate, Object... uriVars) {
         return mockMvc.perform(
             MockMvcRequestBuilders.put(urlTemplate, uriVars)
-                .header(HttpHeaders.AUTHORIZATION, BEARER_TOKEN)
                 .header("ServiceAuthorization", s2sToken)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(toJson(content)));
---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/AuthorisationService.java
@@ -0,0 +1,74 @@
+package uk.gov.hmcts.reform.civil.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserInfo;
+
+import java.util.Arrays;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthorisationService {
+
+    private final ServiceAuthorisationApi serviceAuthorisationApi;
+
+    @Value("${genApp.authorised-services}")
+    private String s2sAuthorisedServices;
+
+    private final IdamClient idamClient;
+
+    private UserInfo userInfo;
+
+    public Boolean authoriseService(String serviceAuthHeader) {
+        String callingService;
+        try {
+            log.info("service auth token header... {}", serviceAuthHeader);
+            String bearerJwt = serviceAuthHeader.startsWith("Bearer ") ? serviceAuthHeader : "Bearer " + serviceAuthHeader;
+            log.info("bearer token header... {}", bearerJwt);
+            callingService = serviceAuthorisationApi.getServiceName(bearerJwt);
+            log.info("Calling Service... {}", callingService);
+            if (callingService != null && Arrays.asList(s2sAuthorisedServices.split(","))
+                .contains(callingService)) {
+
+                return true;
+            }
+        } catch (Exception ex) {
+            //do nothing
+            log.error("S2S token is not authorised" + ex);
+        }
+        return false;
+    }
+
+    public Boolean authoriseUser(String authorisation) {
+        try {
+            userInfo = idamClient.getUserInfo(authorisation);
+            log.info("Userinfo {}", userInfo);
+            if (null != userInfo) {
+                return true;
+            }
+        } catch (Exception ex) {
+            //do nothing
+            log.error("User token is invalid");
+        }
+        return false;
+    }
+
+    public UserInfo getUserInfo() {
+        return this.userInfo;
+    }
+
+    public boolean isServiceAndUserAuthorized(String authorisation, String s2sToken) {
+        return Boolean.TRUE.equals(authoriseUser(authorisation))
+            && Boolean.TRUE.equals(authoriseService(s2sToken));
+    }
+
+    public boolean isServiceAuthorized(String s2sToken) {
+        return Boolean.TRUE.equals(authoriseService(s2sToken));
+    }
+}
