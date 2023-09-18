a10f7418a7f95ee437bd42ccdc6d6efc754158b9
Pijian Liao <Pijian.Liao@HMCTS.NET>
2023-09-18 08:28:02 +0100

    CIV-9787 Application Orders - Free Form

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/finalorder/FreeFormOrderGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/finalorder/FreeFormOrderGenerator.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.civil.service.docmosis.finalorder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.helpers.DateFormatHelper;
 import uk.gov.hmcts.reform.civil.model.CaseData;
 import uk.gov.hmcts.reform.civil.model.docmosis.DocmosisDocument;
@@ -10,11 +10,13 @@ import uk.gov.hmcts.reform.civil.model.docmosis.FreeFormOrder;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.documents.DocumentType;
 import uk.gov.hmcts.reform.civil.model.documents.PDF;
-import uk.gov.hmcts.reform.civil.service.CoreCaseDataService;
 import uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates;
 import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
+import uk.gov.hmcts.reform.civil.service.docmosis.ListGeneratorService;
 import uk.gov.hmcts.reform.civil.service.docmosis.TemplateDataGenerator;
 import uk.gov.hmcts.reform.civil.service.documentmanagement.DocumentManagementService;
+import uk.gov.hmcts.reform.idam.client.IdamClient;
+import uk.gov.hmcts.reform.idam.client.models.UserDetails;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -22,7 +24,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 import static uk.gov.hmcts.reform.civil.enums.dq.OrderOnCourts.ORDER_ON_COURT_INITIATIVE;
 import static uk.gov.hmcts.reform.civil.enums.dq.OrderOnCourts.ORDER_WITHOUT_NOTICE;
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.FREE_FORM_ORDER;
@@ -33,16 +34,21 @@ public class FreeFormOrderGenerator implements TemplateDataGenerator<FreeFormOrd
 
     private final DocumentManagementService documentManagementService;
     private final DocumentGeneratorService documentGeneratorService;
-    private final CoreCaseDataService coreCaseDataService;
+    private final IdamClient idamClient;
+    private final ListGeneratorService listGeneratorService;
+    private final ObjectMapper mapper;
 
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(" d MMMM yyyy");
     private static final String FILE_TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    private static final String ON_COURTS_OWN = "This order is made on court’s own initiative.\n\n";
-    private static final String WITHOUT_NOTICE = "This order is made without notice.\n\n";
 
     public CaseDocument generate(CaseData caseData, String authorisation) {
+        UserDetails userDetails = idamClient.getUserDetails(authorisation);
+        String judgeNameTitle = userDetails.getFullName();
 
         FreeFormOrder templateData = getTemplateData(caseData);
+        Map<String, Object> map = templateData.toMap(mapper);
+        map.put("judgeNameTitle", judgeNameTitle);
+        templateData = mapper.convertValue(map, FreeFormOrder.class);
         DocmosisTemplates template = getTemplate();
         DocmosisDocument document =
                 documentGeneratorService.generateDocmosisDocument(templateData, template);
@@ -58,31 +64,29 @@ public class FreeFormOrderGenerator implements TemplateDataGenerator<FreeFormOrd
 
     @Override
     public FreeFormOrder getTemplateData(CaseData caseData) {
-        CaseDetails parentCase = coreCaseDataService
-                .getCase(Long.parseLong(caseData.getGeneralAppParentCaseLink().getCaseReference()));
-
+        String claimantName = listGeneratorService.claimantsName(caseData);
+        String defendantName = listGeneratorService.defendantsName(caseData);
         return FreeFormOrder.builder()
                 .caseNumber(getCaseNumberFormatted(caseData))
                 .caseName(caseData.getCaseNameHmctsInternal())
                 .receivedDate(getDateFormatted(LocalDate.now()))
-                .claimantReference(getReference(parentCase, "applicantSolicitor1Reference"))
-                .defendantReference(getReference(parentCase, "respondentSolicitor1Reference"))
                 .freeFormRecitalText(caseData.getFreeFormRecitalText())
                 .freeFormOrderedText(caseData.getFreeFormOrderedText())
                 .freeFormOrderValue(getFreeFormOrderValue(caseData))
+                .courtName(caseData.getLocationName())
+                .claimantName(claimantName)
+                .defendantName(defendantName)
                 .build();
     }
 
     protected String getFreeFormOrderValue(CaseData caseData) {
         StringBuilder orderValueBuilder = new StringBuilder();
         if (caseData.getOrderOnCourtsList().equals(ORDER_ON_COURT_INITIATIVE)) {
-            orderValueBuilder.append(ON_COURTS_OWN);
             orderValueBuilder.append(caseData
                     .getOrderOnCourtInitiative().getOnInitiativeSelectionTextArea());
             orderValueBuilder.append(DATE_FORMATTER.format(caseData
                     .getOrderOnCourtInitiative().getOnInitiativeSelectionDate()));
         } else if (caseData.getOrderOnCourtsList().equals(ORDER_WITHOUT_NOTICE)) {
-            orderValueBuilder.append(WITHOUT_NOTICE);
             orderValueBuilder.append(caseData
                     .getOrderWithoutNotice().getWithoutNoticeSelectionTextArea());
             orderValueBuilder.append(DATE_FORMATTER.format(caseData
@@ -95,14 +99,6 @@ public class FreeFormOrderGenerator implements TemplateDataGenerator<FreeFormOrd
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(FILE_TIMESTAMP_FORMAT);
         return String.format(template.getDocumentTitle(),
                 LocalDateTime.now().format(formatter));
-    }
-
-    @SuppressWarnings("unchecked")
-    protected String getReference(CaseDetails caseData, String refKey) {
-        if (nonNull(caseData.getData().get("solicitorReferences"))) {
-            return ((Map<String, String>) caseData.getData().get("solicitorReferences")).get(refKey);
-        }
-        return null;
     }
 
     protected String getCaseNumberFormatted(CaseData caseData) {
