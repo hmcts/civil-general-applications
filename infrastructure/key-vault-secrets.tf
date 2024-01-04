d74070d8d20e978514f6d1882c012c86b04f0c21
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-01-04 14:24:12 +0000

    DTSPO-15909 - migrated Application Insights

--- infrastructure/key-vault-secrets.tf
+++ infrastructure/key-vault-secrets.tf
@@ -1,19 +1,19 @@
 resource "azurerm_key_vault_secret" "appinsights-instrumentation-key" {
   name         = "appinsights-instrumentation-key"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
+  value        = module.application_insights.instrumentation_key
   key_vault_id = module.key-vault.key_vault_id
   content_type = "secret"
   tags = merge(var.common_tags, {
-    "source" : "app insights ${azurerm_application_insights.appinsights.name}"
+    "source" : "app insights ${module.application_insights.name}"
   })
 }
 
 resource "azurerm_key_vault_secret" "appinsights-connection-string" {
   name         = "appinsights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
   key_vault_id = module.key-vault.key_vault_id
   content_type = "secret"
   tags = merge(var.common_tags, {
-    "source" : "appinsights ${azurerm_application_insights.appinsights.name}"
+    "source" : "appinsights ${module.application_insights.name}"
   })
 }
