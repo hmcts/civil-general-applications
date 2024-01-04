94567286048c64732a42a509d060fb6edf413478
Chirag Pareek <111435448+cpareek@users.noreply.github.com>
2024-01-04 17:18:19 +0000

    DTSPO-15909 - migrated Application Insights (#1223)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,32 +12,41 @@ data "azurerm_key_vault" "civil" {
 
 
 # TO DO: REMOVE THIS SECRET AFTER APPLICATION IS CONFGIURED TO FETCH FROM APP SPECIFIC KEY VAULT.
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.component}-${var.env}"
+module "application_insights" {
+  source = "git@github.com:hmcts/terraform-module-application-insights?ref=main"
+
+  env                 = var.env
+  product             = var.product
+  name                = "${var.product}-${var.component}"
   location            = var.appinsights_location
   resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "web"
-  tags                = var.common_tags
+
+  common_tags = var.common_tags
+}
+
+moved {
+  from = azurerm_application_insights.appinsights
+  to   = module.application_insights.azurerm_application_insights.this
 }
 
 # TO DO: REMOVE THIS SECRET AFTER APPLICATION IS CONFGIURED TO FETCH FROM APP SPECIFIC KEY VAULT.
 resource "azurerm_key_vault_secret" "app_insights_instrumental_key" {
   name         = "AppInsightsInstrumentationKeyGeneralApplications"
-  value        = azurerm_application_insights.appinsights.instrumentation_key
+  value        = module.application_insights.instrumentation_key
   key_vault_id = data.azurerm_key_vault.civil.id
   content_type = "secret"
   tags = merge(var.common_tags, {
-    "source" : "app insights ${azurerm_application_insights.appinsights.name}"
+    "source" : "app insights ${module.application_insights.name}"
   })
 }
 
 # TO DO: REMOVE THIS SECRET AFTER APPLICATION IS CONFGIURED TO FETCH FROM APP SPECIFIC KEY VAULT.
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "genapp-appinsights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
+  value        = module.application_insights.connection_string
   key_vault_id = data.azurerm_key_vault.civil.id
   content_type = "secret"
   tags = merge(var.common_tags, {
-    "source" : "appinsights ${azurerm_application_insights.appinsights.name}"
+    "source" : "appinsights ${module.application_insights.name}"
   })
 }
