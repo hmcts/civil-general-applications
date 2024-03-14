f545ed93f8823b7b0c9b7bbb13db5e77c7d153c6
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-03-14 11:52:12 +0000

    Revert "Remove casefileview enabled check"
    This reverts commit 5f161cdcc5dd4365c3756686b79332901d610e91.

--- src/test/java/uk/gov/hmcts/reform/civil/util/AssignCategoryIdTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/util/AssignCategoryIdTest.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.civil.util;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.civil.launchdarkly.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.documents.Document;
@@ -12,12 +13,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.model.documents.DocumentType.GENERAL_ORDER;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 public class AssignCategoryIdTest {
 
     private AssignCategoryId assignCategoryId;
+
+    private FeatureToggleService featureToggleService;
 
     private CaseDocument testCaseDocument = CaseDocument.builder()
         .createdBy("John")
@@ -41,11 +46,28 @@ public class AssignCategoryIdTest {
 
     @BeforeEach
     void setup() {
-        assignCategoryId = new AssignCategoryId();
+        featureToggleService = mock(FeatureToggleService.class);
+        assignCategoryId = new AssignCategoryId(featureToggleService);
+    }
+
+    @Test
+    public void shouldNotAssignCategory_whenInvokedAndToggleFalse() {
+        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(false);
+        List<Element<CaseDocument>> documentList = new ArrayList<>();
+        documentList.add(element(testCaseDocument));
+        assignCategoryId.assignCategoryIdToDocument(testDocument, "testDocumentID");
+        assignCategoryId.assignCategoryIdToCollection(documentList, document -> document.getValue().getDocumentLink(),
+                                                      "testDocumentCollectionID");
+        assignCategoryId.assignCategoryIdToCaseDocument(testCaseDocument, "testCaseDocumentID");
+
+        assertThat(documentList.get(0).getValue().getDocumentLink().getCategoryID()).isNull();
+        assertThat(testDocument.getCategoryID()).isNull();
+        assertThat(testCaseDocument.getDocumentLink().getCategoryID()).isNull();
     }
 
     @Test
     public void shouldAssignCaseDocumentCategoryId_whenInvoked() {
+        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
         assignCategoryId.assignCategoryIdToCaseDocument(testCaseDocument, "testCaseDocumentID");
 
         assertThat(testCaseDocument.getDocumentLink().getCategoryID()).isEqualTo("testCaseDocumentID");
@@ -53,6 +75,7 @@ public class AssignCategoryIdTest {
 
     @Test
     public void shouldAssignDocumentCategoryId_whenInvoked() {
+        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
         assignCategoryId.assignCategoryIdToDocument(testDocument, "testDocumentID");
 
         assertThat(testDocument.getCategoryID()).isEqualTo("testDocumentID");
@@ -60,6 +83,7 @@ public class AssignCategoryIdTest {
 
     @Test
     public void shouldAssignDocumentIdCollection_whenInvoked() {
+        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
         List<Element<CaseDocument>> documentList = new ArrayList<>();
         documentList.add(element(testCaseDocument));
         assignCategoryId.assignCategoryIdToCollection(documentList, document -> document.getValue().getDocumentLink(),
