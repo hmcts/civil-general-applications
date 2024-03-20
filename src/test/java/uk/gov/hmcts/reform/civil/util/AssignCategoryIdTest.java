2d5e3318951baadcf1408af297be5f4afa926464
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-03-20 11:07:42 +0000

    CIV-13173 Assign category Id to case document (#1273)
    * CIV-13173 Assign category Id to case document
    
    * remove casefile view featuretoggle check
    
    * disable test
    
    * CIV-13173 Fix test
    
    * Update AssignCategoryId.java
    
    ---------
    
    Co-authored-by: vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/civil/util/AssignCategoryIdTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/util/AssignCategoryIdTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.civil.util;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import uk.gov.hmcts.reform.civil.launchdarkly.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.documents.Document;
@@ -13,16 +12,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.model.documents.DocumentType.GENERAL_ORDER;
 import static uk.gov.hmcts.reform.civil.utils.ElementUtils.element;
 
 public class AssignCategoryIdTest {
 
     private AssignCategoryId assignCategoryId;
-
-    private FeatureToggleService featureToggleService;
 
     private CaseDocument testCaseDocument = CaseDocument.builder()
         .createdBy("John")
@@ -46,28 +41,11 @@ public class AssignCategoryIdTest {
 
     @BeforeEach
     void setup() {
-        featureToggleService = mock(FeatureToggleService.class);
-        assignCategoryId = new AssignCategoryId(featureToggleService);
-    }
-
-    @Test
-    public void shouldNotAssignCategory_whenInvokedAndToggleFalse() {
-        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(false);
-        List<Element<CaseDocument>> documentList = new ArrayList<>();
-        documentList.add(element(testCaseDocument));
-        assignCategoryId.assignCategoryIdToDocument(testDocument, "testDocumentID");
-        assignCategoryId.assignCategoryIdToCollection(documentList, document -> document.getValue().getDocumentLink(),
-                                                      "testDocumentCollectionID");
-        assignCategoryId.assignCategoryIdToCaseDocument(testCaseDocument, "testCaseDocumentID");
-
-        assertThat(documentList.get(0).getValue().getDocumentLink().getCategoryID()).isNull();
-        assertThat(testDocument.getCategoryID()).isNull();
-        assertThat(testCaseDocument.getDocumentLink().getCategoryID()).isNull();
+        assignCategoryId = new AssignCategoryId();
     }
 
     @Test
     public void shouldAssignCaseDocumentCategoryId_whenInvoked() {
-        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
         assignCategoryId.assignCategoryIdToCaseDocument(testCaseDocument, "testCaseDocumentID");
 
         assertThat(testCaseDocument.getDocumentLink().getCategoryID()).isEqualTo("testCaseDocumentID");
@@ -75,7 +53,6 @@ public class AssignCategoryIdTest {
 
     @Test
     public void shouldAssignDocumentCategoryId_whenInvoked() {
-        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
         assignCategoryId.assignCategoryIdToDocument(testDocument, "testDocumentID");
 
         assertThat(testDocument.getCategoryID()).isEqualTo("testDocumentID");
@@ -83,7 +60,6 @@ public class AssignCategoryIdTest {
 
     @Test
     public void shouldAssignDocumentIdCollection_whenInvoked() {
-        when(featureToggleService.isCaseFileViewEnabled()).thenReturn(true);
         List<Element<CaseDocument>> documentList = new ArrayList<>();
         documentList.add(element(testCaseDocument));
         assignCategoryId.assignCategoryIdToCollection(documentList, document -> document.getValue().getDocumentLink(),
