947747dcc15b07602d7e05809619ad6429e5af76
Mark Drummond <mark.drummond@version1.com>
2024-06-11 15:47:54 +0100

    fixed test

--- src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisServiceTest.java
@@ -2,12 +2,9 @@ package uk.gov.hmcts.reform.civil.service.docmosis;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.civil.enums.CaseCategory;
 import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.enums.dq.GAByCourtsInitiativeGAspec;
