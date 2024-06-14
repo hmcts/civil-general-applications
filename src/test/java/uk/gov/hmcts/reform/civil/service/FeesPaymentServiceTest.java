9c79945122512213ed490f2c9a4591584b82f34f
kenneth-hmcts <kenneth.lai@HMCTS.NET>
2024-06-14 17:26:47 +0100

    CIV-9465 Set up request Dto

--- src/test/java/uk/gov/hmcts/reform/civil/service/FeesPaymentServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/FeesPaymentServiceTest.java
@@ -62,7 +62,7 @@ class FeesPaymentServiceTest {
     private static final CardPaymentServiceRequestDTO CARD_PAYMENT_SERVICE_REQUEST
         = CardPaymentServiceRequestDTO.builder()
         .returnUrl("${cui-front-end.url}/general-app-payment-confirmation/1701090368574910")
-        .amount(new BigDecimal("232.00")).currency("GBP").build();
+        .amount(new BigDecimal("232.00")).currency("GBP").language("En").build();
 
     @Autowired
     private FeesPaymentService feesPaymentService;
