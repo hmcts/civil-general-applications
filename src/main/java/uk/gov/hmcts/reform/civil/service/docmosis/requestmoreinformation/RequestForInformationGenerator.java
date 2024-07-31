5a79c0f8aed5e191856125d51dbf682a37c08ca2
Manish Garg <manish.garg@hmcts.net>
2024-07-31 11:23:56 +0100

    CIV-14127 Court Response Document - Without notice becomes with notice (#1400)
    * CIV-14127 Added logic to generate document when judge uncloakes the without notice app.
    
    * CIV-14127 Added logic to generate document when judge uncloakes the without notice app.
    
    * CIV-14127 Added logic to generate document when judge uncloakes the without notice app.
    
    * CIV-14127 Added logic to generate document when judge uncloakes the without notice app.
    
    * CIV-14127 Added logic to generate document when judge uncloakes the without notice app.
    
    * CIV-14127 Added logic to generate document when judge uncloakes the without notice app.
    
    ---------
    
    Co-authored-by: pliao-hmcts <113367232+pliao-hmcts@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/requestmoreinformation/RequestForInformationGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/requestmoreinformation/RequestForInformationGenerator.java
@@ -2,23 +2,32 @@ package uk.gov.hmcts.reform.civil.service.docmosis.requestmoreinformation;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.civil.enums.dq.GAJudgeRequestMoreInfoOption;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Fee;
 import uk.gov.hmcts.reform.civil.model.docmosis.DocmosisDocument;
 import uk.gov.hmcts.reform.civil.model.docmosis.judgedecisionpdfdocument.JudgeDecisionPdfDocument;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.documents.DocumentType;
 import uk.gov.hmcts.reform.civil.model.documents.PDF;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAJudicialRequestMoreInfo;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAPbaDetails;
+import uk.gov.hmcts.reform.civil.service.GaForLipService;
 import uk.gov.hmcts.reform.civil.service.docmosis.DocmosisService;
 import uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates;
 import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
 import uk.gov.hmcts.reform.civil.service.docmosis.TemplateDataGenerator;
 import uk.gov.hmcts.reform.civil.service.documentmanagement.DocumentManagementService;
+import uk.gov.hmcts.reform.civil.utils.MonetaryConversions;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.REQUEST_FOR_INFORMATION;
+import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.REQUEST_FOR_INFORMATION_SEND_TO_OTHER_PARTY;
 
 @Service
 @RequiredArgsConstructor
@@ -27,11 +36,13 @@ public class RequestForInformationGenerator implements TemplateDataGenerator<Jud
     private final DocumentManagementService documentManagementService;
     private final DocumentGeneratorService documentGeneratorService;
     private final DocmosisService docmosisService;
+    private final GaForLipService gaForLipService;
 
     public CaseDocument generate(CaseData caseData, String authorisation) {
         JudgeDecisionPdfDocument templateData = getTemplateData(caseData, authorisation);
 
-        DocmosisTemplates docmosisTemplate = getDocmosisTemplate();
+        DocmosisTemplates docmosisTemplate = getDocmosisTemplate(caseData);
+        DocumentType documentType = getDocumentType(caseData);
 
         DocmosisDocument docmosisDocument = documentGeneratorService.generateDocmosisDocument(
             templateData,
@@ -41,7 +52,7 @@ public class RequestForInformationGenerator implements TemplateDataGenerator<Jud
         return documentManagementService.uploadDocument(
             authorisation,
             new PDF(getFileName(docmosisTemplate), docmosisDocument.getBytes(),
-                    DocumentType.REQUEST_FOR_INFORMATION)
+                    documentType)
         );
     }
 
@@ -68,12 +79,44 @@ public class RequestForInformationGenerator implements TemplateDataGenerator<Jud
                 .judgeRecital(caseData.getJudicialDecisionRequestMoreInfo().getJudgeRecitalText())
                 .judgeComments(caseData.getJudicialDecisionRequestMoreInfo().getJudgeRequestMoreInfoText())
                 .submittedOn(LocalDate.now())
-                .dateBy(caseData.getJudicialDecisionRequestMoreInfo().getJudgeRequestMoreInfoByDate());
+                .dateBy(caseData.getJudicialDecisionRequestMoreInfo().getJudgeRequestMoreInfoByDate())
+                .additionalApplicationFee(getAdditionalApplicationFee(caseData))
+                .applicationCreatedDate(caseData.getCreatedDate().toLocalDate());
+            ;
 
         return judgeDecisionPdfDocumentBuilder.build();
     }
 
-    private DocmosisTemplates getDocmosisTemplate() {
+    private DocmosisTemplates getDocmosisTemplate(CaseData caseData) {
+        GAJudgeRequestMoreInfoOption gaJudgeRequestMoreInfoOption = getGAJudgeRequestMoreInfoOption(caseData);
+
+        if (gaForLipService.isLipApp(caseData) && gaJudgeRequestMoreInfoOption == GAJudgeRequestMoreInfoOption.SEND_APP_TO_OTHER_PARTY) {
+            return REQUEST_FOR_INFORMATION_SEND_TO_OTHER_PARTY;
+        }
         return REQUEST_FOR_INFORMATION;
+    }
+
+    private DocumentType getDocumentType(CaseData caseData) {
+        GAJudgeRequestMoreInfoOption gaJudgeRequestMoreInfoOption = getGAJudgeRequestMoreInfoOption(caseData);
+        if (gaForLipService.isLipApp(caseData) && gaJudgeRequestMoreInfoOption == GAJudgeRequestMoreInfoOption.SEND_APP_TO_OTHER_PARTY) {
+            return DocumentType.SEND_APP_TO_OTHER_PARTY;
+        }
+        return DocumentType.REQUEST_FOR_INFORMATION;
+    }
+
+    private String getAdditionalApplicationFee(CaseData caseData) {
+        return Optional.ofNullable(caseData.getGeneralAppPBADetails()).map(
+            GAPbaDetails::getFee).map(Fee::getCalculatedAmountInPence)
+            .map(MonetaryConversions::penniesToPounds)
+            .map(amount -> amount.setScale(2))
+            .map(BigDecimal::toPlainString)
+            .map(amount -> amount.replace(".00", ""))
+            .map(amount -> "£" + amount)
+            .orElse("£" + BigDecimal.ZERO.toPlainString());
+    }
+
+    private GAJudgeRequestMoreInfoOption getGAJudgeRequestMoreInfoOption(CaseData caseData) {
+        return Optional.ofNullable(caseData.getJudicialDecisionRequestMoreInfo())
+            .map(GAJudicialRequestMoreInfo::getRequestMoreInfoOption).orElse(null);
     }
 }
