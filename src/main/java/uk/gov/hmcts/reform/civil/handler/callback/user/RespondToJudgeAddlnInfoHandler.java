0af900d3044acabf989ee75ecc15e21a3850cece
Pijian Liao <Pijian.Liao@HMCTS.NET>
2024-08-12 16:26:26 +0100

    CIV-14276 add generated text file to doc collection

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToJudgeAddlnInfoHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToJudgeAddlnInfoHandler.java
@@ -12,7 +12,12 @@ import uk.gov.hmcts.reform.civil.callback.CaseEvent;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.BusinessProcess;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.common.Element;
+import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
+import uk.gov.hmcts.reform.civil.model.documents.Document;
+import uk.gov.hmcts.reform.civil.service.docmosis.requestmoreinformation.RespondForInformationGenerator;
 import uk.gov.hmcts.reform.civil.utils.DocUploadUtils;
+import uk.gov.hmcts.reform.civil.utils.ElementUtils;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 
 import java.util.Collections;
@@ -33,6 +38,7 @@ public class RespondToJudgeAddlnInfoHandler extends CallbackHandler {
     private final ObjectMapper objectMapper;
     private final CaseDetailsConverter caseDetailsConverter;
     private final IdamClient idamClient;
+    private final RespondForInformationGenerator respondForInformationGenerator;
 
     private static final List<CaseEvent> EVENTS = Collections.singletonList(RESPOND_TO_JUDGE_ADDITIONAL_INFO);
 
@@ -50,11 +56,14 @@ public class RespondToJudgeAddlnInfoHandler extends CallbackHandler {
         String userId = idamClient.getUserInfo(callbackParams.getParams().get(BEARER_TOKEN).toString()).getUid();
         CaseData.CaseDataBuilder caseDataBuilder = caseData.toBuilder();
         String role = DocUploadUtils.getUserRole(caseData, userId);
+        List<Element<Document>> tobeAdded = caseData.getGeneralAppAddlnInfoUpload();
         if (Objects.nonNull(caseData.getGeneralAppAddlnInfoText())) {
-
+            CaseDocument caseDocument = respondForInformationGenerator.generate(caseData,
+                    callbackParams.getParams().get(BEARER_TOKEN).toString(), role);
+            tobeAdded.add(ElementUtils.element(caseDocument.getDocumentLink()));
         }
         DocUploadUtils.addDocumentToAddl(caseData, caseDataBuilder,
-                caseData.getGeneralAppAddlnInfoUpload(), role, CaseEvent.RESPOND_TO_JUDGE_ADDITIONAL_INFO, false);
+                tobeAdded, role, CaseEvent.RESPOND_TO_JUDGE_ADDITIONAL_INFO, false);
         caseDataBuilder.generalAppAddlnInfoUpload(Collections.emptyList());
         caseDataBuilder.businessProcess(BusinessProcess.ready(RESPOND_TO_JUDGE_ADDITIONAL_INFO)).build();
         caseDataBuilder.generalAppAddlnInfoText(null);
---
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/requestmoreinformation/RespondForInformationGenerator.java
@@ -0,0 +1,87 @@
+package uk.gov.hmcts.reform.civil.service.docmosis.requestmoreinformation;
+
+import static uk.gov.hmcts.reform.civil.callback.CaseEvent.RESPOND_TO_JUDGE_ADDITIONAL_INFO;
+import static uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates.HEARING_APPLICATION;
+
+import uk.gov.hmcts.reform.civil.enums.CaseRole;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.docmosis.DocmosisDocument;
+import uk.gov.hmcts.reform.civil.model.docmosis.judgedecisionpdfdocument.JudgeDecisionPdfDocument;
+import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
+import uk.gov.hmcts.reform.civil.model.documents.DocumentType;
+import uk.gov.hmcts.reform.civil.model.documents.PDF;
+import uk.gov.hmcts.reform.civil.service.docmosis.DocmosisTemplates;
+import uk.gov.hmcts.reform.civil.service.docmosis.DocumentGeneratorService;
+import uk.gov.hmcts.reform.civil.service.docmosis.TemplateDataGenerator;
+import uk.gov.hmcts.reform.civil.service.documentmanagement.DocumentManagementService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RespondForInformationGenerator implements TemplateDataGenerator<JudgeDecisionPdfDocument> {
+
+    private final DocumentManagementService documentManagementService;
+    private final DocumentGeneratorService documentGeneratorService;
+    private String role;
+
+    public CaseDocument generate(CaseData caseData, String authorisation, String role) {
+        this.role = role;
+        JudgeDecisionPdfDocument templateData = getTemplateData(caseData, authorisation);
+
+        DocmosisTemplates docmosisTemplate = getTemplate();
+
+        DocmosisDocument docmosisDocument = documentGeneratorService.generateDocmosisDocument(
+                templateData,
+                docmosisTemplate
+        );
+
+        return documentManagementService.uploadDocument(
+                authorisation,
+                new PDF(getFileName(docmosisTemplate), docmosisDocument.getBytes(),
+                        DocumentType.REQUEST_FOR_INFORMATION)
+        );
+    }
+
+    @Override
+    public JudgeDecisionPdfDocument getTemplateData(CaseData caseData, String authorisation) {
+
+        JudgeDecisionPdfDocument.JudgeDecisionPdfDocumentBuilder judgeDecisionPdfDocumentBuilder =
+                JudgeDecisionPdfDocument.builder()
+                        .claimNumber(caseData.getCcdCaseReference().toString())
+                        .claimant1Name(caseData.getClaimant1PartyName())
+                        .defendant1Name(caseData.getDefendant1PartyName())
+                        .judgeComments(caseData.getGeneralAppAddlnInfoText())
+                        .judgeNameTitle(getSubmittedBy(role, caseData))
+                        .submittedOn(LocalDate.now());
+        ;
+
+        return judgeDecisionPdfDocumentBuilder.build();
+    }
+
+    private String getSubmittedBy(String role, CaseData caseData) {
+        if (role.equals(CaseRole.CLAIMANT.getFormattedName())) {
+            return caseData.getApplicantPartyName();
+        } else {
+            if (caseData.getParentClaimantIsApplicant() == YesOrNo.YES) {
+                return caseData.getDefendant1PartyName();
+            }
+            return caseData.getClaimant1PartyName();
+        }
+    }
+
+    private String getFileName(DocmosisTemplates docmosisTemplate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return String.format(docmosisTemplate.getDocumentTitle(), LocalDateTime.now().format(formatter));
+    }
+
+    protected DocmosisTemplates getTemplate() {
+        return HEARING_APPLICATION;
+    }
+}
