e042a1ce0ace12d0d59438b68730f0749b1cebd8
neeta-hmcts <neeta.shinde@hmcts.net>
2024-08-23 15:19:47 +0100

    draft changes.

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/DashboardScenarios.java
@@ -8,7 +8,9 @@ public enum DashboardScenarios {
     SCENARIO_AAA6_GENERAL_APPLICATION_CREATED_CLAIMANT("Scenario.AAA6.GeneralApplication.Created.Claimant"),
     SCENARIO_AAA6_GENERAL_APPLICATION_CREATED_DEFENDANT("Scenario.AAA6.GeneralApplication.Created.Defendant"),
     SCENARIO_AAA6_GENERAL_APPLICATION_COMPLETE_CLAIMANT("Scenario.AAA6.GeneralApplication.Complete.Claimant"),
-    SCENARIO_AAA6_GENERAL_APPLICATION_COMPLETE_DEFENDANT("Scenario.AAA6.GeneralApplication.Complete.Defendant");
+    SCENARIO_AAA6_GENERAL_APPLICATION_COMPLETE_DEFENDANT("Scenario.AAA6.GeneralApplication.Complete.Defendant"),
+    SCENARIO_AAA6_GENERAL_APPLICATION_ISSUED("Scenario.AAA6.GeneralApplication.Issue.Payment.Required"),
+    SCENARIO_AAA6_GENERAL_APPLICATION_SUBMITTED("Scenario.AAA6.GeneralApplication.Submitted");
 
     private final String scenario;
 
