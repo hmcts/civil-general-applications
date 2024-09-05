37085257ddd44e03f3a335496c60af50d004af3a
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2024-09-05 13:09:13 +0100

    CIV-14404 Added new event to send a dashboard notification after application fee is paid

---
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/ApplicationSubmittedDashboardNotificationHandler.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications;
+
+import uk.gov.hmcts.reform.civil.callback.CaseEvent;
+import uk.gov.hmcts.reform.civil.callback.DashboardCallbackHandler;
+import uk.gov.hmcts.reform.civil.client.DashboardApiClient;
+import uk.gov.hmcts.reform.civil.launchdarkly.FeatureToggleService;
+import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.service.DashboardNotificationsParamsMapper;
+
+import java.util.List;
+
+import static uk.gov.hmcts.reform.civil.handler.callback.camunda.dashboardnotifications.DashboardScenarios.SCENARIO_AAA6_GENERAL_APPLICATION_SUBMITTED_APPLICANT;
+
+public class ApplicationSubmittedDashboardNotificationHandler extends DashboardCallbackHandler {
+
+    private static final List<CaseEvent> EVENTS = List.of(CaseEvent.UPDATE_GA_DASHBOARD_NOTIFICATION);
+
+    public ApplicationSubmittedDashboardNotificationHandler(DashboardApiClient dashboardApiClient,
+                                                                DashboardNotificationsParamsMapper mapper,
+                                                                FeatureToggleService featureToggleService) {
+        super(dashboardApiClient, mapper, featureToggleService);
+    }
+
+    @Override
+    public List<CaseEvent> handledEvents() {
+        return EVENTS;
+    }
+
+    @Override
+    public String getScenario(CaseData caseData) {
+        return SCENARIO_AAA6_GENERAL_APPLICATION_SUBMITTED_APPLICANT.getScenario();
+    }
+
+}
