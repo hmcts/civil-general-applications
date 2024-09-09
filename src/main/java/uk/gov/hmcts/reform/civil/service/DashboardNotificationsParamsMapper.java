a12ed099346549479e6d1c497356ccecb620a679
paul-pearson <paul.pearson@version1.com>
2024-09-09 16:35:20 +0100

    CIV-14407 CIV-14408 Set request more info deadline param

--- src/main/java/uk/gov/hmcts/reform/civil/service/DashboardNotificationsParamsMapper.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/DashboardNotificationsParamsMapper.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.genapplication.GAJudicialRequestMoreInfo;
 import uk.gov.hmcts.reform.civil.utils.DateUtils;
 
 @Service
@@ -21,12 +22,22 @@ public class DashboardNotificationsParamsMapper {
             params.put("generalAppNotificationDeadlineDateEn", DateUtils.formatDate(date));
             params.put("generalAppNotificationDeadlineDateCy", DateUtils.formatDateInWelsh(date));
         });
+
+        getJudgeRequestMoreInfoByDate(caseData).ifPresent(date -> {
+            params.put("judgeRequestMoreInfoByDateEn", DateUtils.formatDate(date));
+            params.put("judgeRequestMoreInfoByDateCy", DateUtils.formatDateInWelsh(date));
+        });
         return params;
     }
 
     private static Optional<LocalDate> getGeneralAppNotificationDeadlineDate(CaseData caseData) {
         return Optional.ofNullable(caseData.getGeneralAppNotificationDeadlineDate())
             .map(LocalDateTime::toLocalDate);
+    }
+
+    private static Optional<LocalDate> getJudgeRequestMoreInfoByDate(CaseData caseData) {
+        return Optional.ofNullable(caseData.getJudicialDecisionRequestMoreInfo())
+            .map(GAJudicialRequestMoreInfo::getJudgeRequestMoreInfoByDate);
     }
 
 }
