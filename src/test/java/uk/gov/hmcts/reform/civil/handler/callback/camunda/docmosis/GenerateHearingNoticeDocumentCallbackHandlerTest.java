c7227ef5e2fbb952fab7a637b5b83befa420d977
karthick mohanasundaram <92928238+karthick-mohanasundaram-HMCTS@users.noreply.github.com>
2024-10-02 08:02:02 +0100

    Add Unit test for Hearing Notice

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateHearingNoticeDocumentCallbackHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/docmosis/GenerateHearingNoticeDocumentCallbackHandlerTest.java
@@ -9,10 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.ccd.client.model.AboutToStartOrSubmitCallbackResponse;
+import uk.gov.hmcts.reform.ccd.client.model.CaseDetails;
 import uk.gov.hmcts.reform.civil.callback.CallbackParams;
+import uk.gov.hmcts.reform.civil.enums.YesOrNo;
 import uk.gov.hmcts.reform.civil.handler.callback.BaseCallbackHandlerTest;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.GeneralAppParentCaseLink;
 import uk.gov.hmcts.reform.civil.model.documents.CaseDocument;
 import uk.gov.hmcts.reform.civil.model.documents.Document;
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
@@ -25,6 +28,8 @@ import uk.gov.hmcts.reform.civil.utils.AssignCategoryId;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static uk.gov.hmcts.reform.civil.callback.CallbackType.ABOUT_TO_SUBMIT;
 
 @ExtendWith(SpringExtension.class)
@@ -77,5 +82,31 @@ class GenerateHearingNoticeDocumentCallbackHandlerTest extends BaseCallbackHandl
         var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
         CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
         assertThat(updatedData.getHearingNoticeDocument().size()).isEqualTo(1);
+    }
+
+    @Test
+    void shouldGenerateHearingNoticeDocumentWithCoverLetter() {
+        CaseDocument caseDocument = CaseDocument.builder()
+            .documentLink(Document.builder().documentUrl("doc").build()).build();
+
+        when(hearingFormGenerator.generate(any(), any())).thenReturn(caseDocument);
+        when(gaForLipService.isLipApp(any())).thenReturn(true);
+        when(gaForLipService.isLipResp(any())).thenReturn(true);
+        when(hearingFormGenerator.generate(any(), any(), any(), any())).thenReturn(caseDocument);
+
+        when(coreCaseDataService.getCase(any())).thenReturn(CaseDetails.builder().build());
+        when(caseDetailsConverter.toCaseData(any())).thenReturn(CaseData.builder().build());
+        CaseData caseData = CaseDataBuilder.builder().generalOrderApplication()
+            .applicationIsUncloakedOnce(YesOrNo.YES)
+            .generalAppParentCaseLink(GeneralAppParentCaseLink.builder().caseReference("1234").build())
+            .build();
+        CallbackParams params = callbackParamsOf(caseData, ABOUT_TO_SUBMIT);
+
+        var response = (AboutToStartOrSubmitCallbackResponse) handler.handle(params);
+        CaseData updatedData = mapper.convertValue(response.getData(), CaseData.class);
+        assertThat(updatedData.getHearingNoticeDocument().size()).isEqualTo(1);
+        verify(hearingFormGenerator, times(1)).generate(any(), any());
+        verify(hearingFormGenerator, times(2)).generate(any(), any(), any(), any());
+        verify(sendFinalOrderPrintService, times(2)).sendJudgeFinalOrderToPrintForLIP(any(), any(), any(), any(), any());
     }
 }
