b59a435fcf422be75b9c7b725dc6031d38c6c92b
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2024-10-03 14:54:49 +0100

    CIV-15078 changes

--- src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/CertOfSC.java
+++ src/main/java/uk/gov/hmcts/reform/civil/model/citizenui/CertOfSC.java
@@ -5,9 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import uk.gov.hmcts.reform.civil.model.DebtPaymentEvidence;
+import uk.gov.hmcts.reform.civil.model.common.Element;
 import uk.gov.hmcts.reform.civil.model.documents.Document;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Builder(toBuilder = true)
@@ -17,5 +19,5 @@ public class CertOfSC {
 
     private LocalDate defendantFinalPaymentDate;
     private DebtPaymentEvidence debtPaymentEvidence;
-    private Document proofOfDebtDoc;
+    private List<Element<Document>> proofOfDebtDoc;
 }
