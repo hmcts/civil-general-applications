3eb48bcac6ff4b1487890d6914bf90cf1b63af36
tiagreaney <tiagreaney@gmail.com>
2024-10-24 14:32:51 +0100

    checkstyleMain and checkstyleTest fix

--- src/main/java/uk/gov/hmcts/reform/civil/request/SimpleCallbackRequest.java
+++ src/main/java/uk/gov/hmcts/reform/civil/request/SimpleCallbackRequest.java
@@ -1,4 +1,5 @@
 package uk.gov.hmcts.reform.civil.request;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
