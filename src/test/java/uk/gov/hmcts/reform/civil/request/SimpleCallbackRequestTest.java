8efb49aa086517f72f5d000131a05c4831869783
tiagreaney <tiagreaney@gmail.com>
2024-10-28 12:42:59 +0000

    add test coverage

--- src/test/java/uk/gov/hmcts/reform/civil/request/SimpleCallbackRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/request/SimpleCallbackRequestTest.java
@@ -1,4 +1,5 @@
 package uk.gov.hmcts.reform.civil.request;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
