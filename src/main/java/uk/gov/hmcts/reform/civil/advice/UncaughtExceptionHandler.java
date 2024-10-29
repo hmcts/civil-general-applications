6cdddbc8608fed9de16ba738af5a1af6f888752c
tiagreaney <tiagreaney@gmail.com>
2024-10-29 16:32:06 +0000

    add log

--- src/main/java/uk/gov/hmcts/reform/civil/advice/UncaughtExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/advice/UncaughtExceptionHandler.java
@@ -21,7 +21,7 @@ public class UncaughtExceptionHandler {
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity<Object> runtimeException(Exception exception) {
         log.debug(exception.getMessage(), exception);
-        log.info(Arrays.toString(exception.getStackTrace()));
+        log.info("failed ", exception);
         String errorMessage = "Runtime exception of type %s occurred with message: %s for case %s run by user %s";
         log.error(errorMessage.formatted(exception.getClass().getName(), exception.getMessage(),
                                          requestData.caseId(), requestData.userId()));
