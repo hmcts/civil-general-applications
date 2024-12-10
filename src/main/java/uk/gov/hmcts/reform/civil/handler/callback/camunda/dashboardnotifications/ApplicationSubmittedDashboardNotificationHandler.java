394f3764f6c77d0266d4d290a431fb39dfc4e702
neeta-hmcts <neeta.shinde@hmcts.net>
2024-12-10 10:13:53 +0000

    remove a check to display full fee paid notification for full/part/no remission event.

--- src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/ApplicationSubmittedDashboardNotificationHandler.java
+++ src/main/java/uk/gov/hmcts/reform/civil/handler/callback/camunda/dashboardnotifications/ApplicationSubmittedDashboardNotificationHandler.java
@@ -51,14 +51,12 @@ public class ApplicationSubmittedDashboardNotificationHandler extends CallbackHa
 
     public List<String> getScenarios(CaseData caseData) {
         List<String> scenarios = new ArrayList<>();
-        if (Objects.nonNull(caseData.getGaHwfDetails())
-            && (caseData.getGaHwfDetails().getHwfCaseEvent() != CaseEvent.NO_REMISSION_HWF_GA)) {
-            if (caseData.claimIssueFullRemissionNotGrantedHWF(caseData)) {
-                scenarios.add(SCENARIO_AAA6_GENERAL_APPS_HWF_FEE_PAID_APPLICANT.getScenario());
-            } else {
-                scenarios.add(SCENARIO_AAA6_GENERAL_APPS_HWF_FULL_REMISSION_APPLICANT.getScenario());
-            }
+        if (caseData.claimIssueFullRemissionNotGrantedHWF(caseData)) {
+            scenarios.add(SCENARIO_AAA6_GENERAL_APPS_HWF_FEE_PAID_APPLICANT.getScenario());
+        } else {
+            scenarios.add(SCENARIO_AAA6_GENERAL_APPS_HWF_FULL_REMISSION_APPLICANT.getScenario());
         }
+
         scenarios.add(SCENARIO_AAA6_GENERAL_APPLICATION_SUBMITTED_APPLICANT.getScenario());
         return scenarios;
     }
