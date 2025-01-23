8cf7a067c35f7f5ea7541faa19039e76dce644e5
drummondjm <mark.drummond@version1.com>
2025-01-23 18:47:43 +0000

    Refactored into single API call and added test

--- src/main/java/uk/gov/hmcts/reform/civil/service/roleassignment/RolesAndAccessAssignmentService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/roleassignment/RolesAndAccessAssignmentService.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.civil.service.roleassignment;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.civil.config.SystemUpdateUserConfiguration;
 import uk.gov.hmcts.reform.civil.ras.model.GrantType;
@@ -24,7 +23,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class RolesAndAccessAssignmentService {
 
     public static final List<String> ROLE_TYPE = List.of("CASE");
@@ -50,18 +49,12 @@ public class RolesAndAccessAssignmentService {
                 if (response.isEmpty()) {
                     log.info("Role assignment list empty for case ID, and allocated roles {}", mainCaseId);
                 } else {
-                    log.info("GET ROLES case id roleAssignmentResponse:  {}", response);
-                    Map<String, List<RoleAssignmentResponse>> roleAssignmentsByActorId = response.stream()
-                        .collect(Collectors.groupingBy(RoleAssignmentResponse::getActorId));
-
-                    roleAssignmentsByActorId.forEach((actorId, actorRoles) ->
-                                                         actorRoles.forEach(role -> assignRoles(gaCaseId, role, bearerToken))
-                    );
+                    assignRoles(gaCaseId, roleAssignmentResponse, bearerToken);
                 }
             }, () -> log.info("No role assignment response found for case ID {}", mainCaseId));
     }
 
-    private void assignRoles(String gaCaseId, RoleAssignmentResponse roleToAssign, String bearerToken) {
+    private void assignRoles(String gaCaseId, RoleAssignmentServiceResponse rolesToAssign, String bearerToken) {
         String systemUserId = userService.getUserInfo(bearerToken).getUid();
 
         roleAssignmentService.assignUserRoles(
@@ -72,45 +65,38 @@ public class RolesAndAccessAssignmentService {
                                  .assignerId(systemUserId)
                                  .replaceExisting(false)
                                  .build())
-                .requestedRoles(buildRoleAssignments(gaCaseId, roleToAssign)).build());
+                .requestedRoles(buildRoleAssignments(gaCaseId, rolesToAssign)).build());
         log.info("Assigned roles successfully");
     }
 
-    private static RoleAssignment buildRoleAssignment(String gaCaseId, RoleAssignmentResponse roleToAssign) {
-
-        Map<String, Object> roleAssignmentAttributes = new HashMap<>();
-        roleAssignmentAttributes.put("caseId", gaCaseId);
-        roleAssignmentAttributes.put("caseType", "GENERALAPPLICATION");
-        roleAssignmentAttributes.put("jurisdiction", "CIVIL");
-        if (roleToAssign.getAttributes().getContractType() != null) {
-            roleAssignmentAttributes.put("contractType", roleToAssign.getAttributes().getContractType());
-        }
-        if (roleToAssign.getAttributes().getRegion() != null) {
-            roleAssignmentAttributes.put("region", roleToAssign.getAttributes().getRegion());
-        }
-        if (roleToAssign.getAttributes().getPrimaryLocation() != null) {
-            roleAssignmentAttributes.put("primaryLocation", roleToAssign.getAttributes().getPrimaryLocation());
-        }
-
-        return RoleAssignment.builder()
-            .actorId(roleToAssign.getActorId())
-            .actorIdType(roleToAssign.getActorIdType())
-            .grantType(GrantType.valueOf(roleToAssign.getGrantType()))
-            .roleCategory(RoleCategory.valueOf(roleToAssign.getRoleCategory()))
-            .roleType(RoleType.valueOf(roleToAssign.getRoleType()))
-            .classification(roleToAssign.getClassification())
-            .roleName(roleToAssign.getRoleName())
-            .beginTime(roleToAssign.getBeginTime())
-            .endTime(roleToAssign.getEndTime())
-            .readOnly(false)
-            .attributes(roleAssignmentAttributes)
-            .build();
-    }
-
-    private static List<RoleAssignment> buildRoleAssignments(String gaCaseId, RoleAssignmentResponse roleToAssign) {
-        RoleAssignment roleAssignment = buildRoleAssignment(gaCaseId, roleToAssign);
+    private static List<RoleAssignment> buildRoleAssignments(String gaCaseId, RoleAssignmentServiceResponse roleToAssign) {
         List<RoleAssignment> roleAssignments = new ArrayList<>();
-        roleAssignments.add(roleAssignment);
+        Map<String, List<RoleAssignmentResponse>> roleAssignmentsByActorId = roleToAssign.getRoleAssignmentResponse().stream()
+            .collect(Collectors.groupingBy(RoleAssignmentResponse::getActorId));
+
+        roleAssignmentsByActorId.forEach((actorId, roleResponses) -> {
+            roleResponses.forEach(roleResponse -> {
+
+                Map<String, Object> roleAssignmentAttributes = new HashMap<>();
+                roleAssignmentAttributes.put("caseId", gaCaseId);
+                roleAssignmentAttributes.put("caseType", "GENERALAPPLICATION");
+                roleAssignmentAttributes.put("jurisdiction", "CIVIL");
+
+                roleAssignments.add(RoleAssignment.builder()
+                                        .actorId(roleResponse.getActorId())
+                                        .actorIdType(roleResponse.getActorIdType())
+                                        .grantType(GrantType.valueOf(roleResponse.getGrantType()))
+                                        .roleCategory(RoleCategory.valueOf(roleResponse.getRoleCategory()))
+                                        .roleType(RoleType.valueOf(roleResponse.getRoleType()))
+                                        .classification(roleResponse.getClassification())
+                                        .roleName(roleResponse.getRoleName())
+                                        .beginTime(roleResponse.getBeginTime())
+                                        .endTime(roleResponse.getEndTime())
+                                        .readOnly(false)
+                                        .attributes(roleAssignmentAttributes)
+                                        .build());
+            });
+        });
         return roleAssignments;
     }
 
