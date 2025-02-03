0e824cd147ea4f847d4f44208df6ad554be5b39f
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-02-03 21:34:24 +0000

    civ-16553 corrected check style issues

--- src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToJudgeAddlnInfoHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/handler/callback/user/RespondToJudgeAddlnInfoHandlerTest.java
@@ -38,7 +38,6 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
