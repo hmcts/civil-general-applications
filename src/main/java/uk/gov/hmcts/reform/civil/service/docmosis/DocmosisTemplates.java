74ddd5f78f8d70add7f7b4de9c9e2069f93df36e
jeswanth-hmcts <134285996+jeswanth-hmcts@users.noreply.github.com>
2025-02-14 13:44:15 +0000

    Update DocmosisTemplates.java

--- src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/docmosis/DocmosisTemplates.java
@@ -14,7 +14,7 @@ public enum DocmosisTemplates {
     FREE_FORM_ORDER("CV-UNS-GAP-ENG-01075.docx", GENERAL_ORDER.getDocumentTitle()),
     ASSISTED_ORDER_FORM("CV-UNS-GAP-ENG-01076.docx", GENERAL_ORDER.getDocumentTitle()),
     CONSENT_ORDER_FORM("CV-UNS-GAP-ENG-01078.docx", "Consent_order_for_application_%s.pdf"),
-    GENERAL_APPLICATION_DRAFT("CV-UNS-GAP-ENG-01558.docx", "Draft_application_%s.pdf"),
+    GENERAL_APPLICATION_DRAFT("CV-UNS-GAP-ENG-01077.docx", "Draft_application_%s.pdf"),
     REQUEST_FOR_INFORMATION_SEND_TO_OTHER_PARTY("CV-SPC-GAP-ENG-SEND-TO-OTHER-PARTY.docx", "make-with-notice_%s.pdf"),
     RESPOND_FOR_INFORMATION("CV-UNS-GAP-ENG-01079.docx", "Respond_for_information_for_application_%s.pdf"),
     RESPOND_FOR_WRITTEN_REPRESENTATION("CV-SPC-GAP-ENG-01080.docx", "Respond_for_written_representation_for_application_%s.pdf"),
