4d3c35003a6109ef9a3803805a8ec4d80fb38634
paul-pearson <paul.pearson@version1.com>
2025-04-10 14:46:41 +0100

    CIV-16986 Fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/civil/service/JudicialApplicantNotificationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/service/JudicialApplicantNotificationServiceTest.java
@@ -49,7 +49,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.civil.enums.CaseState.ORDER_MADE;
 import static uk.gov.hmcts.reform.civil.enums.PaymentStatus.SUCCESS;
