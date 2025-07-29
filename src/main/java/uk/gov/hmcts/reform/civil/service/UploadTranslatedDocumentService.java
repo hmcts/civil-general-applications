92109d031994fd64fd0cf582f485c2a75159638c
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2025-07-29 15:44:28 +0100

    CIV-16895 added missing condition for approve edit

--- src/main/java/uk/gov/hmcts/reform/civil/service/UploadTranslatedDocumentService.java
+++ src/main/java/uk/gov/hmcts/reform/civil/service/UploadTranslatedDocumentService.java
@@ -205,7 +205,8 @@ public class UploadTranslatedDocumentService {
                     preTranslationWrittenRepsConcurrent.ifPresent(bulkPrintOriginalDocuments::add);
                     caseDataBuilder.writtenRepConcurrentDocument(writtenRepsConcurrentDocs);
                     caseDataBuilder.originalDocumentsBulkPrint(bulkPrintOriginalDocuments);
-                } else if (document.getValue().getDocumentType().equals(GENERAL_ORDER)) {
+                } else if (document.getValue().getDocumentType().equals(GENERAL_ORDER)
+                || document.getValue().getDocumentType().equals(APPROVE_OR_EDIT_ORDER)) {
                     Optional<Element<CaseDocument>> preTranslationGeneralOrder = preTranslationGaDocuments.stream()
                         .filter(item -> item.getValue().getDocumentType() == DocumentType.GENERAL_ORDER)
                         .findFirst();
