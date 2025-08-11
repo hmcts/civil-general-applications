aef22fac9722d963aad7af563de1d35d790bdec7
Neeta Shinde <neeta.shinde@hmcts.net>
2025-08-11 11:19:36 +0100

    fixed build error

--- src/test/java/uk/gov/hmcts/reform/civil/stateflow/StateFlowEngineTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/stateflow/StateFlowEngineTest.java
@@ -10,6 +10,7 @@ import uk.gov.hmcts.reform.civil.enums.dq.GAJudgeMakeAnOrderOption;
 import uk.gov.hmcts.reform.civil.helpers.CaseDetailsConverter;
 import uk.gov.hmcts.reform.civil.launchdarkly.FeatureToggleService;
 import uk.gov.hmcts.reform.civil.model.CaseData;
+import uk.gov.hmcts.reform.civil.model.Fee;
 import uk.gov.hmcts.reform.civil.model.PaymentDetails;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAInformOtherParty;
 import uk.gov.hmcts.reform.civil.model.genapplication.GAJudgesHearingListGAspec;
@@ -20,6 +21,8 @@ import uk.gov.hmcts.reform.civil.model.genapplication.GARespondentOrderAgreement
 import uk.gov.hmcts.reform.civil.sampledata.CaseDataBuilder;
 import uk.gov.hmcts.reform.civil.service.flowstate.StateFlowEngine;
 import uk.gov.hmcts.reform.civil.stateflow.model.State;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -114,7 +117,8 @@ public class StateFlowEngineTest {
                 GAPbaDetails.builder()
                     .paymentDetails(PaymentDetails.builder()
                                         .status(PaymentStatus.SUCCESS)
-                                        .build()).build())
+                                        .build())
+                    .fee(Fee.builder().code("FEE23").build()).build())
             .generalAppInformOtherParty(GAInformOtherParty.builder()
                                             .isWithNotice(YES).build())
             .parentClaimantIsApplicant(YES)
@@ -141,7 +145,8 @@ public class StateFlowEngineTest {
                 GAPbaDetails.builder()
                     .paymentDetails(PaymentDetails.builder()
                                         .status(PaymentStatus.SUCCESS)
-                                        .build()).build())
+                                        .build())
+                    .fee(Fee.builder().code("FEE23").build()).build())
             .generalAppInformOtherParty(GAInformOtherParty.builder()
                                             .isWithNotice(YES).build())
             .parentClaimantIsApplicant(YES)
@@ -296,7 +301,7 @@ public class StateFlowEngineTest {
 
         assertThat(stateFlow.getState()).extracting(State::getName).isNotNull()
             .isEqualTo(PROCEED_GENERAL_APPLICATION.fullName());
-        assertThat(stateFlow.getFlags()).hasSize(4);
+        assertThat(stateFlow.getFlags()).hasSize(5);
         assertThat(stateFlow.getFlags().get("WELSH_ENABLED")).isTrue();
         assertThat(stateFlow.getStateHistory()).hasSize(3)
             .extracting(State::getName)
@@ -319,7 +324,7 @@ public class StateFlowEngineTest {
 
         assertThat(stateFlow.getState()).extracting(State::getName).isNotNull()
             .isEqualTo(PROCEED_GENERAL_APPLICATION.fullName());
-        assertThat(stateFlow.getFlags()).hasSize(4);
+        assertThat(stateFlow.getFlags()).hasSize(5);
         assertThat(stateFlow.getFlags().get("WELSH_ENABLED")).isTrue();
         assertThat(stateFlow.getStateHistory()).hasSize(3)
             .extracting(State::getName)
@@ -336,7 +341,8 @@ public class StateFlowEngineTest {
                 GAPbaDetails.builder()
                     .paymentDetails(PaymentDetails.builder()
                                         .status(PaymentStatus.SUCCESS)
-                                        .build()).build())
+                                        .build())
+                    .fee(Fee.builder().code("FEE23").build()).build())
             .generalAppInformOtherParty(GAInformOtherParty.builder()
                                             .isWithNotice(YES).build())
             .isGaApplicantLip(YES)
@@ -373,7 +379,7 @@ public class StateFlowEngineTest {
 
         assertThat(stateFlow.getState()).extracting(State::getName).isNotNull()
             .isEqualTo(PROCEED_GENERAL_APPLICATION.fullName());
-        assertThat(stateFlow.getFlags()).hasSize(4);
+        assertThat(stateFlow.getFlags()).hasSize(5);
         assertThat(stateFlow.getFlags().get("WELSH_ENABLED")).isFalse();
         assertThat(stateFlow.getStateHistory()).hasSize(3)
             .extracting(State::getName)
@@ -450,7 +456,8 @@ public class StateFlowEngineTest {
                 GAPbaDetails.builder()
                     .paymentDetails(PaymentDetails.builder()
                                         .status(PaymentStatus.SUCCESS)
-                                        .build()).build())
+                                        .build())
+                    .fee(Fee.builder().code("FEE23").build()).build())
             .generalAppInformOtherParty(GAInformOtherParty.builder()
                                             .isWithNotice(YES).build())
             .parentClaimantIsApplicant(YES)
@@ -481,7 +488,8 @@ public class StateFlowEngineTest {
                 GAPbaDetails.builder()
                     .paymentDetails(PaymentDetails.builder()
                                         .status(PaymentStatus.SUCCESS)
-                                        .build()).build())
+                                        .build())
+                    .fee(Fee.builder().code("FEE23").build()).build())
             .generalAppInformOtherParty(GAInformOtherParty.builder()
                                             .isWithNotice(YES).build())
             .parentClaimantIsApplicant(YES)
@@ -600,7 +608,7 @@ public class StateFlowEngineTest {
 
         assertThat(stateFlow.getState()).extracting(State::getName).isNotNull()
             .isEqualTo(PROCEED_GENERAL_APPLICATION.fullName());
-        assertThat(stateFlow.getFlags()).hasSize(4);
+        assertThat(stateFlow.getFlags()).hasSize(5);
         assertThat(stateFlow.getFlags().get("WELSH_ENABLED")).isFalse();
         assertThat(stateFlow.getStateHistory()).hasSize(3)
             .extracting(State::getName)
