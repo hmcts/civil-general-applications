d779abb0ccd76b51a081a19065a012157ff36f59
Gareth Lancaster <90632240+Gareth40343@users.noreply.github.com>
2025-09-26 08:08:16 +0100

    CIV-17703 QM Clean up (#1835)
    * Removed LD call for public queries toggle
    
    * Update FeatureToggleServiceTest.java
    
    Fixed unit tests
    
    ---------
    
    Co-authored-by: GarethLancaster <31533575+Gareth40342@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/civil/launchdarkly/FeatureToggleServiceTest.java
@@ -151,32 +151,29 @@ class FeatureToggleServiceTest {
         verifyBoolVariationCalled(organisationOnboardedFeatureKey, List.of("timestamp", "environment", "orgId"));
     }
 
-    @ParameterizedTest
-    @ValueSource(booleans = {true, false})
-    void shouldReturnCorrectValue_whenPublicQueryEnabledLr(Boolean toggleStat) {
-        var lrPublicQuery = "public-query-management";
-        givenToggle(lrPublicQuery, toggleStat);
-
+    @Test
+    void shouldReturnCorrectValue_whenNonLipCase() {
         CaseData caseData = CaseDataBuilder.builder().withNoticeDraftAppCaseData();
-
-        assertThat(featureToggleService.isPublicQueryManagementEnabled(caseData)).isEqualTo(toggleStat);
+        assertThat(featureToggleService.isPublicQueryManagementEnabled(caseData)).isEqualTo(true);
     }
 
     @ParameterizedTest
     @CsvSource({
-        "true,NO,YES,NO",
-        "true,YES,NO,NO",
-        "true,YES,YES,YES",
-        "false,NO,NO,NO",
+        "true,NO,YES",
+        "true,YES,NO",
+        "true,YES,YES",
+        "false,NO,YES",
+        "false,YES,NO",
+        "false,YES,YES",
     })
-    void shouldReturnCorrectValue_whenPublicQueryEnabledLip(boolean toggleStat, String applicant1Represented,
-                                                            String respondent1Represented, String respondent2Represented) {
+    void shouldReturnCorrectValue_whenCuiQueryManagementEnabledLip(boolean toggleStat, YesOrNo applicant1Lip,
+                                                            YesOrNo respondent1Lip) {
         var multipartyFeatureKey = "cui-query-management";
         givenToggle(multipartyFeatureKey, toggleStat);
         CaseData caseData = CaseDataBuilder.builder().withNoticeDraftAppCaseData()
             .toBuilder()
-            .isGaApplicantLip(Enum.valueOf(YesOrNo.class, applicant1Represented))
-            .isGaRespondentOneLip(Enum.valueOf(YesOrNo.class, respondent1Represented))
+            .isGaApplicantLip(applicant1Lip)
+            .isGaRespondentOneLip(respondent1Lip)
             .mainCaseSubmittedDate(LocalDateTime.of(LocalDate.now(), LocalTime.NOON))
             .build();
 
